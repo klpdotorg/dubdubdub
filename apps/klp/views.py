36a90f5ca3185658f025fe8d589d34fe33ee9fe2
Sanjay B <b@pad.ma>
2014-03-22 17:17:47 +0530

    migrate api view to using class based views, following structure of DISE app

--- apps/klp/views.py
+++ apps/klp/views.py
@@ -1,20 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import View
 from django.contrib.gis.geos import Polygon
 #from coords.models import InstCoord
 from klp.models import School, InstCoord
-import json
-from django.http import HttpResponse
+from common.views import JSONResponseMixin
 
-def schools(request):
-    bbox_string = request.GET.get("bounds")
-    bbox = Polygon.from_bbox([float(b) for b in bbox_string.split(",")])
-    schools = School.objects.filter(instcoord__coord__within=bbox).select_related('instcoord')
-    count = schools.count()
-    d = {
-        'type': 'FeatureCollection',
-        'count': count,
-        'features': [s.get_geojson() for s in schools]
-    }
-    return HttpResponse(json.dumps(d))
 
-# Create your views here.
+class Schools(View, JSONResponseMixin):
+
+    def get(self, *args, **kwargs):
+        bbox_string = self.request.GET.get("bounds")
+        bbox = Polygon.from_bbox([float(b) for b in bbox_string.split(",")])
+        schools = School.objects.filter(instcoord__coord__within=bbox).select_related('instcoord')
+        count = schools.count()
+        context = {
+            'type': 'FeatureCollection',
+            'count': count,
+            'features': [s.get_geojson() for s in schools]
+        }
+        return self.render_to_response(context)
+
+
+# def schools(request):
+#     bbox_string = request.GET.get("bounds")
+#     bbox = Polygon.from_bbox([float(b) for b in bbox_string.split(",")])
+#     schools = School.objects.filter(instcoord__coord__within=bbox).select_related('instcoord')
+#     count = schools.count()
+#     d = {
+#         'type': 'FeatureCollection',
+#         'count': count,
+#         'features': [s.get_geojson() for s in schools]
+#     }
+#     return HttpResponse(json.dumps(d))
+
+# # Create your views here.
--- dubdubdub/urls.py
+++ dubdubdub/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
-
+from klp.views import Schools
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^api/v1/schools', 'klp.views.schools'),
+    url(r'^api/v1/schools', Schools.as_view(), name='api_schools'),
 )
