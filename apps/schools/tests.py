f4fb88fddcbd4a4c1a8850985b9a3f5e1c598c94
Sanjay B <b@pad.ma>
2014-05-13 15:52:29 +0530

    add some tests for pagination, existence of more properties

--- apps/schools/tests.py
+++ apps/schools/tests.py
@@ -18,6 +18,7 @@ class ApiTestCase(unittest.TestCase):
         results = json.loads(response.content)
         self.assertEqual(len(results['features']), 50, "got 50 schools as first page")
         sample_school = results['features'][0]
+        self.assertTrue(sample_school['properties'].has_key('id'), "has a property called id")
         self.assertTrue(sample_school['properties'].has_key('name'), "has a property called name")
 
     def test_api_schools_info(self):
@@ -26,4 +27,15 @@ class ApiTestCase(unittest.TestCase):
         results = json.loads(response.content)
         self.assertEqual(len(results['features']), 50, "got 50 schools as first page")
         sample_school = results['features'][0]
+        self.assertTrue(sample_school['properties'].has_key('management'), 'has a property called management')
+        self.assertTrue(sample_school['properties'].has_key('category'), 'has a property called category')
+        self.assertTrue(sample_school['properties'].has_key('address'), 'has a property called address')
         self.assertTrue(sample_school['properties'].has_key('dise_code'), "has a property called dise_code")
+        response_page2 = self.c.get("/api/v1/schools/info?page=2")
+        self.assertEqual(response_page2.status_code, 200, "schools info page 2 status code is 200")
+        results_page2 = json.loads(response_page2.content)
+        self.assertEqual(len(results_page2['features']), 50, "got 50 schools as second page")
+        sample_school_2 = results_page2['features'][0]
+        school1id = sample_school['properties']['id']
+        school2id = sample_school_2['properties']['id']
+        self.assertFalse(school1id == school2id, "page 2 differs from page 1")
