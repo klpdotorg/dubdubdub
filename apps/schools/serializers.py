41f8829ffb65eea4f5a2cd920308e9ad9c2bab31
Bibhas <iambibhas@gmail.com>
2014-05-29 12:42:35 +0530

    returning boundary

--- apps/schools/serializers.py
+++ apps/schools/serializers.py
@@ -13,10 +13,11 @@ class SchoolInfoSerializer(KLPGeoSerializer):
     dise_code = serializers.IntegerField(source='dise_info_id')
     type_id = serializers.CharField(source='boundary.type_id')
     address_full = serializers.CharField(source='address.full')
+    landmark = serializers.CharField(source='address.landmark')
 
     class Meta:
         model = School
-        fields = ('id', 'name', 'mgmt', 'cat', 'moi', 'sex', 'address_full',
+        fields = ('id', 'name', 'mgmt', 'cat', 'moi', 'sex', 'address_full', 'landmark',
             'dise_code', 'type_id', 'geometry')
 
 
