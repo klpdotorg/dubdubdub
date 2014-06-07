75abfe1785b6b593ffec3aad82a79cf26e4755dc
Bibhas <iambibhas@gmail.com>
2014-06-07 22:39:43 +0530

    more cleanup

--- sql/cleanup.sql
+++ sql/cleanup.sql
@@ -1,1 +1,7 @@
 UPDATE "tb_school" SET "status" = 0 WHERE UPPER("tb_school"."name"::text) LIKE UPPER('%DELETE%');
+UPDATE "tb_school" SET "status" = 0 WHERE UPPER("tb_school"."name"::text) LIKE UPPER('%test%');
+UPDATE "tb_school" SET "status" = 0 WHERE UPPER("tb_school"."name"::text) LIKE UPPER('%tst%');
+UPDATE "tb_school" SET "status" = 0 WHERE UPPER("tb_school"."name"::text) LIKE UPPER('%tset%');
+UPDATE "tb_school" SET name=regexp_replace(name, E'\t', '', 'g') WHERE name LIKE E'%\t%';
+UPDATE "tb_school" SET name=regexp_replace(name, '\s+', ' ', 'g') WHERE name LIKE E'%  %';
+UPDATE "tb_school" SET "status" = 0 WHERE "tb_school"."name" IN ('1', '2');
