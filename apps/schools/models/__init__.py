2e7eea642a9d9fe1b1f5710e39187a17e743fb2e
Sanjay B <b@pad.ma>
2014-06-27 02:15:18 +0530

    pep8 linting for app/schools/models/__init__.py

--- apps/schools/models/__init__.py
+++ apps/schools/models/__init__.py
@@ -1,23 +1,23 @@
-from .education import AcademicYear, Address, BoundaryHierarchy, Boundary, BoundaryType, \
-    Child, StudentGroup, School, Student, StudentStudentGroup, Teacher, TeacherStudentGroup, \
-    TeacherQualification, BoundaryPrimarySchool, SchoolDetails
+from .education import AcademicYear, Address, BoundaryHierarchy, Boundary,\
+    BoundaryType, Child, StudentGroup, School, Student, StudentStudentGroup,\
+    Teacher, TeacherStudentGroup, TeacherQualification, BoundaryPrimarySchool,\
+    SchoolDetails
 
-from .assessments import Assessment, InstitutionAgg, InstitutionAssessmentAgg, \
-    InstitutionAssessmentAggCohorts, InstitutionAssessmentGenderSinglescore, \
-    InstitutionAssessmentMtSinglescore, InstitutionAssessmentReadingAggCohorts, \
-    InstitutionAssessmentSinglescore, InstitutionAssessmentSinglescoreGender, \
-    InstitutionAssessmentSinglescoreMt, InstitutionBasicAssessmentInfo, \
-    InstitutionBasicAssessmentInfoCohorts, Partner, PreschoolAssessmentAgg, \
-    PreschoolBasicAssessmentInfo, Programme, Question, SchoolAgg, \
-    SchoolAssessmentAgg, SchoolBasicAssessmentInfo, StudentEval, SchoolEval, \
+from .assessments import Assessment, InstitutionAgg, InstitutionAssessmentAgg,\
+    InstitutionAssessmentAggCohorts, InstitutionAssessmentGenderSinglescore,\
+    InstitutionAssessmentMtSinglescore,\
+    InstitutionAssessmentReadingAggCohorts,\
+    InstitutionAssessmentSinglescore,\
+    InstitutionAssessmentSinglescoreGender,\
+    InstitutionAssessmentSinglescoreMt, InstitutionBasicAssessmentInfo,\
+    InstitutionBasicAssessmentInfoCohorts, Partner, PreschoolAssessmentAgg,\
+    PreschoolBasicAssessmentInfo, Programme, Question, SchoolAgg,\
+    SchoolAssessmentAgg, SchoolBasicAssessmentInfo, StudentEval, SchoolEval,\
     AngInfraAgg, AngDisplayMaster
 
 from .coords import InstCoord, BoundaryCoord, Assembly, Parliament, Postal
 
-from .partners import DiseDisplayMaster, DiseFacilityAgg, DiseInfo, PaisaData, \
+from .partners import DiseDisplayMaster, DiseFacilityAgg, DiseInfo, PaisaData,\
     DiseRteAgg, LibBorrow, LibLangAgg, LibLevelAgg, Libinfra, MdmAgg
 
 from .elected_reps import ElectedrepMaster, SchoolElectedrep
-
-
-
