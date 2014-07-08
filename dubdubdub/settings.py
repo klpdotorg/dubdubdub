6cf2ddcdd42e4425c8256a547dd2ea1d928d769f
Sanjay Bhangar <sanjay@therateof.com>
2014-07-08 14:15:47 +0000

    add djang-rest-swagger for auto-documentation, made available at /api-docs/

--- dubdubdub/settings.py
+++ dubdubdub/settings.py
@@ -152,6 +152,7 @@ INSTALLED_APPS = (
     'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     # 'debug_toolbar',
     #'raven.contrib.django.raven_compat',
 
--- dubdubdub/urls.py
+++ dubdubdub/urls.py
@@ -59,4 +59,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^api/v1/', include('dubdubdub.api_urls')),
+    url(r'^api-docs/', include('rest_framework_swagger.urls'))
 )
