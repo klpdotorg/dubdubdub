9bd9a93b03eb74ad6c7b06ef231e7ee1a91d1e67
subhaklp <subha@klp.org.in>
2014-07-30 18:55:59 +0530

    More unit tests work..
    Organized it into folders etc..

--- apps/users/tests/test_users2.py
+++ apps/users/tests/test_users2.py
@@ -4,7 +4,7 @@ from rest_framework.test import APITestCase
 from django.core.urlresolvers import reverse
 from rest_framework import status
 from django.conf import settings
-from .models import User
+from users.models import User
 import unittest
 import json
 import csv
@@ -19,83 +19,82 @@ class UsersApiTestCase2(APITestCase):
                 'mobile_no': '9837543232',
                 'password' : 'deleteme2'
             }
+    test_user2_information = {
+                'email': 'iamtestbot2@test.com',
+                'first_name': 'iamtestbot2',
+                'last_name': 'iamtestbot2',
+                'mobile_no': '9872321123',
+                'password' : 'iamtestbot2'
+            }
+    test_user1_token=''
 
-    def setup(self):
+    def setUp(self):
     	print 'setup'
-    	self.url="/api/v1/users"
+        #Create a user user1 for usage in all the tests.
+        response=self.client.post(self.usersBaseUrl,self.test_user1_information)
+        print 'Status code:' + str(response.status_code)
+        print response.data
+        self.test_user1_token = response.data['token']
+
+
 
     #Test attempts to create a test user via the REST API
     def test_create_user_success(self):
 
         print 'RUNNING test_create_user_success...'
         
-        response=self.client.post(self.usersBaseUrl,self.test_user1_information)
+        response=self.client.post(self.usersBaseUrl,self.test_user2_information)
+        print response.status_code
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED,"User created")
+        print response.data
+
+        self.delete_user(self.test_user2_information['email'])
+        print 'DONE'
+        print '---------------------------------------'
+	
+    def test_user_login(self):
+
+        print 'Testing user login'
+        response=self.client.post(self.usersBaseUrl,self.test_user2_information)
         print response.status_code
         self.assertEqual(response.status_code,status.HTTP_201_CREATED,"User created")
         print response.data
 
         #Now, test logging in as this user with username/pwd
-        #print "TESTING user login"
-        #userLoginInfo={'email': 'deleteme2@test.com','password':'deleteme2'}
-        #response = self.client.post(self.usersBaseUrl+"/login",userLoginInfo)
-        #print response.status_code
-        #self.assertEqual(response.status_code,status.HTTP_200_OK)
-        #print response.data
+        userLoginInfo={'email': self.test_user2_information['email'],'password': self.test_user2_information['password']}
+        response = self.client.post(self.usersBaseUrl+"/login",userLoginInfo)
+        print response.status_code
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        print response.data
 
-        self.delete_user(self.test_user1_information['email'])
+        self.delete_user(self.test_user2_information['email'])
         print 'DONE'
         print '---------------------------------------'
-	
+
+
     #Regular users should not be able to GET users information. It should fail with a 403 even if token is supplied
     def test_regular_user_GET_forbidden(self):
-    	print 'testing users sample 2'
-    	url = '/api/v1/users'
-        print 'RUNNING test_create_user_success...'
-        userInformation = {
-                'email': 'deleteme2@test.com',
-                'first_name': 'deleteme2',
-                'last_name': 'deleteme2',
-                'mobile_no': '9837543232',
-                'password' : 'deleteme2'
-            }
-        response=self.client.post(url,userInformation)
-        print response.status_code
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        token = response.data['token']
-
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response=self.client.get(url)
+        print 'Testing regular user GET forbidden'
+        print 'User1 token is:' + self.test_user1_token
+        #Get the user1's token and set it as credentials on the client
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.test_user1_token)
+        response=self.client.get(self.usersBaseUrl)
         print response.status_code
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         print response.data
 
     #Regular users should not be able to GET if no token is supplied. 401 should result
     def test_regular_user_GET_unauthorized(self):
-    	print 'testing users sample 2'
-    	url = '/api/v1/users'
-        print 'RUNNING test_create_user_success...'
-        userInformation = {
-                'email': 'deleteme2@test.com',
-                'first_name': 'deleteme2',
-                'last_name': 'deleteme2',
-                'mobile_no': '9837543232',
-                'password' : 'deleteme2'
-            }
-        response=self.client.post(url,userInformation)
-        print response.status_code
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        #token = response.data['token']
-
-        #self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-        response=self.client.get(url)
+    
+        print 'RUNNING test regular user GET unauthorized...'
+        
+        response=self.client.get(self.usersBaseUrl)
         print response.status_code
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         print response.data
 
     def test_super_user_GET(self):
     	print 'testing super user GET'
-    	url = '/api/v1/users'
-        print 'RUNNING test_create_user_success...'
         userInformation = {
                 'mobile_no': '9837543232',
                 'first_name': 'superuser',
@@ -108,23 +107,24 @@ class UsersApiTestCase2(APITestCase):
         self.adminUser.is_superuser = True
         self.adminUser.save()
         
+        print 'Getting all users..'
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.adminToken)
-        response=self.client.get(url)
+        response=self.client.get(self.usersBaseUrl)
         print response.status_code
         print response.data
         self.assertEqual(response.status_code,status.HTTP_200_OK)
 
-        print "Getting the user profile"
-        response=self.client.get(url+"/profile")
+        print "Getting the user profile for the super user"
+        response=self.client.get(self.usersBaseUrl+"/profile")
         print response.status_code
         print response.data
         self.assertEqual(response.status_code,status.HTTP_200_OK)
 
+        print "Deleting super user.."
         self.delete_user('test_superuser@test.com')
 
     def test_super_user_GET_notoken(self):
         print 'testing super user GET without token'
-        url = '/api/v1/users'
         userInformation = {
                 'mobile_no': '9837543232',
                 'first_name': 'superuser',
@@ -138,7 +138,7 @@ class UsersApiTestCase2(APITestCase):
         self.adminUser.save()
         
         # Don't set the token. Super user should still get unauthorized
-        response=self.client.get(url)
+        response=self.client.get(self.usersBaseUrl)
         print response.status_code
         print response.data
         self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
@@ -147,16 +147,16 @@ class UsersApiTestCase2(APITestCase):
 
     def delete_user(self, user_email):
         u = User.objects.get(email=user_email)
-        print u.email
+        print 'Deleting user --' + u.email
         u.delete()
     
         
     def tearDown(self):
         print "calling teardown..."
-        #u = User.objects.get(email='test_superuser@test.com')
-        #print u.email
-        #u.delete()
-        #print "User deleted"
+        u = User.objects.get(email=self.test_user1_information['email'])
+        email=u.email
+        u.delete()
+        print "User deleted -- " + email
 		
     
 		
