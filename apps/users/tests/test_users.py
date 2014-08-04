c5caef625739b88e1d0bb2910b9ef9fcff33f627
subhaklp <subha@klp.org.in>
2014-08-04 12:59:01 +0530

    More changes to tests..
    Added a few more tests..

--- apps/users/tests/test_users.py
+++ apps/users/tests/test_users.py
@@ -9,9 +9,13 @@ import unittest
 import json
 import csv
 
-class UsersApiTestCase2(APITestCase):
+class UsersApiTestCase(APITestCase):
 
     usersBaseUrl="/api/v1/users"
+    loginUrl="/api/v1/users/login"
+    logoutUrl="/api/v1/users/logout"
+    profilesUrl="/api/v1/users/profile"
+
     test_user1_information = {
                 'email': 'deleteme2@test.com',
                 'first_name': 'deleteme2',
@@ -32,9 +36,10 @@ class UsersApiTestCase2(APITestCase):
     	print 'setup'
         #Create a user user1 for usage in all the tests.
         response=self.client.post(self.usersBaseUrl,self.test_user1_information)
-        print 'Status code:' + str(response.status_code)
+        print 'Status code:' + str(response.status_code) +'; Expected: ' + str(status.HTTP_201_CREATED)
         print response.data
         self.test_user1_token = response.data['token']
+        print '---------------------------------------'
 
 
 
@@ -44,31 +49,67 @@ class UsersApiTestCase2(APITestCase):
         print 'RUNNING test_create_user_success...'
         
         response=self.client.post(self.usersBaseUrl,self.test_user2_information)
-        print response.status_code
+        print 'POST ' + self.usersBaseUrl
+        print 'Status code:' + str(response.status_code) +'; Expected: ' + str(status.HTTP_201_CREATED)
         self.assertEqual(response.status_code,status.HTTP_201_CREATED,"User created")
-        print response.data
+        print 'Response:' + str(response.data)
 
         self.delete_user(self.test_user2_information['email'])
         print 'DONE'
         print '---------------------------------------'
 	
+    #Tests user login using an already created user
     def test_user_login(self):
 
         print 'Testing user login'
         response=self.client.post(self.usersBaseUrl,self.test_user2_information)
-        print response.status_code
+        print 'POST ' + self.usersBaseUrl
+        print 'Status code:' + str(response.status_code) +'; Expected: ' + str(status.HTTP_201_CREATED)
         self.assertEqual(response.status_code,status.HTTP_201_CREATED,"User created")
-        print response.data
+        print 'Response:'+ str(response.data)
 
         #Now, test logging in as this user with username/pwd
+
         userLoginInfo={'email': self.test_user2_information['email'],'password': self.test_user2_information['password']}
-        response = self.client.post(self.usersBaseUrl+"/login",userLoginInfo)
-        print response.status_code
+        response = self.client.post(self.loginUrl,userLoginInfo)
+        print 'POST ' + self.loginUrl
+        print 'Status code:' + str(response.status_code)  +'; Expected: ' + str(status.HTTP_200_OK)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
-        print response.data
+        print 'Response:' + str(response.data)
+
+        #Get profile
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + str(response.data['token']))
+        response=self.client.get(self.profilesUrl)
+        print 'GET ' + self.profilesUrl
+        print 'Status code:' + str(response.status_code)  +'; Expected: ' + str(status.HTTP_200_OK)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+        #Test logout
+        response=self.client.get(self.logoutUrl)
+        print 'GET ' + self.logoutUrl
+        print 'Status code: ' + str(response.status_code)+'; Expected: ' + str(status.HTTP_200_OK)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        print 'Response:' + str(response.data)
+
+        #Now test fetching profiles, it should return a 401
+        #Clear out credentials from the client
+        self.client.credentials()
+        response=self.client.get(self.profilesUrl)
+        print 'GET ' + self.profilesUrl
+        print 'Status code: ' + str(response.status_code) +'; Expected: ' + str(status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
 
         self.delete_user(self.test_user2_information['email'])
         print 'DONE'
+        print '---------------------------------------'
+
+    #Users should get a 401 if attempting to fetch profiles without login
+    def test_user_getprofile_nologin(self):
+        print "Testing user GET profile without login"
+        print 'GET ' + self.profilesUrl
+        response=self.client.get(self.profilesUrl)  
+        print 'Status code: ' + str(response.status_code)+'; Expected: ' + str(status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
         print '---------------------------------------'
 
 
@@ -78,20 +119,29 @@ class UsersApiTestCase2(APITestCase):
         print 'User1 token is:' + self.test_user1_token
         #Get the user1's token and set it as credentials on the client
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.test_user1_token)
+
+        print 'GET ' + self.usersBaseUrl
         response=self.client.get(self.usersBaseUrl)
-        print response.status_code
+        print 'Status code: ' + str(response.status_code)+'; Expected: ' + str(status.HTTP_403_FORBIDDEN)
+
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         print response.data
+        print '---------------------------------------'
+
 
     #Regular users should not be able to GET if no token is supplied. 401 should result
     def test_regular_user_GET_unauthorized(self):
     
         print 'RUNNING test regular user GET unauthorized...'
         
+        print 'GET ' + self.usersBaseUrl
+
         response=self.client.get(self.usersBaseUrl)
-        print response.status_code
+        print 'Status code: ' + str(response.status_code)+'; Expected: ' + str(status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        print response.data
+        print 'Response: ' + str(response.data)
+        print '---------------------------------------'
+
 
     def test_super_user_GET(self):
     	print 'testing super user GET'
@@ -122,6 +172,8 @@ class UsersApiTestCase2(APITestCase):
 
         print "Deleting super user.."
         self.delete_user('test_superuser@test.com')
+        print '---------------------------------------'
+
 
     def test_super_user_GET_notoken(self):
         print 'testing super user GET without token'
@@ -139,9 +191,10 @@ class UsersApiTestCase2(APITestCase):
         
         # Don't set the token. Super user should still get unauthorized
         response=self.client.get(self.usersBaseUrl)
-        print response.status_code
+        print 'Status code: ' + str(response.status_code) +'; Expected: ' + str(status.HTTP_401_UNAUTHORIZED)
         print response.data
         self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
+        print '---------------------------------------'
     
 
 
