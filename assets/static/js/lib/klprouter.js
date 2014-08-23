9f9aeeab0cc1b3e2387f871eced1ab866688a1a1
Sanjay B <b@pad.ma>
2014-08-23 17:23:00 +0530

    add setHash method to router + convert vars to camelCase in router

--- assets/static/js/lib/klprouter.js
+++ assets/static/js/lib/klprouter.js
@@ -2,32 +2,81 @@ var KLPRouter = function(routes) {
     this.routes = routes || {};
     var that = this;
 
-    this.hashChanged = function() {
-        var hash = window.location.hash.substr(1, window.location.hash.length-1);
-        var query_params = {};
-        var hash_split = hash.split("?");
+    var getQueryParams = function(hash) {
+        var queryParams = {};
+        var hashSplit = hash.split("?");
 
-        if (hash_split.length > 1) {
-            hash = hash_split[0];
-            var query_params_string = hash_split[1];
-            var query_params_split = query_params_string.split("&");
+        if (hashSplit.length > 1) {
+            hash = hashSplit[0];
+            var queryParamsString = hashSplit[1];
+            var queryParamsSplit = queryParamsString.split("&");
 
-            for (var i = 0; i < query_params_split.length; i++) {
-                var temp = query_params_split[i].split("=");
+            for (var i = 0; i < queryParamsSplit.length; i++) {
+                var temp = queryParamsSplit[i].split("=");
                 if (temp.length == 2){
-                    query_params[temp[0]] = temp[1];
+                    queryParams[temp[0]] = temp[1];
                 }
             };
         }
+        return queryParams;       
+    };
+
+    var getQueryString = function(queryObj) {
+        var paramsArray = [];
+        for (var param in queryObj) {
+            if (queryObj.hasOwnProperty(param)) {
+                var s = param + '=' + queryObj[param];
+                paramsArray.push(s);
+            }
+        }
+        return paramsArray.join('&');
+    };
+
+    this.hashChanged = function() {
+        var hash = window.location.hash.substr(1, window.location.hash.length-1);
+        var queryParams = getQueryParams(hash);
 
         for (pattern in routes) {
             var regex = new RegExp(pattern, "i");
             var matches = regex.exec(hash);
 
             if (matches !== null && matches.length > 0) {
-                routes[pattern](matches, query_params);
+                routes[pattern](matches, queryParams);
             }
         }
+    };
+
+    this.setHash = function(url, queryParams, options) {
+        /*
+            url <string>: new url base fragment
+            queryParams <obj>: object of query params to change
+            options: obj
+                - trigger <boolean> whether to trigger hashchange event
+                - replace <boolean> whether to 'replace' URL without
+                        creating event in history
+                replace can be set to true only if trigger is false.
+         */
+        var defaults = {
+            'trigger': true,
+            'replace': false
+        };
+        var opts = $.extend(defaults, options);
+        var hash = window.location.hash.substr(1, window.location.hash.length-1);
+        var hashSplit = hash.split("?");
+        if (!url) {
+            url = hashSplit[0];
+        }
+        var currentParams = getQueryParams(hash);
+        var newParams = $.extend(currentParams, queryParams);
+        var newHash = url + '?' + getQueryString(newParams);
+        if (opts.trigger) {
+            location.hash = newHash;
+        } else if (!opts.replace) {
+            history.pushState(null, null, '#' + newHash);
+        } else {
+            history.replaceState(null, null, '#' + newHash);
+        }
+
     };
 
     this.init = function() {
