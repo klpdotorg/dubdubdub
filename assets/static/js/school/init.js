c7c9e752f9b5d3cbc24b083c092fcedb99d671bd
Sanjay B <b@pad.ma>
2014-08-25 01:01:29 +0530

    get geometry

--- assets/static/js/school/init.js
+++ assets/static/js/school/init.js
@@ -3,17 +3,16 @@
         klp.router = new KLPRouter({});
         klp.router.init();
         klp.tabs.init();
-        var $infoXHR = klp.api.do("schools/school/" + SCHOOL_ID);
+        var $infoXHR = klp.api.do("schools/school/" + SCHOOL_ID, {geometry: 'yes'});
         $infoXHR.done(function(data) {
             console.log("info", data);
             var tpl = swig.compile($('#tpl-school-info').html());
             //var html = swig.render($('#tpl-school-info').html(), {locals: data});
-            var html = tpl(data);
+            var html = tpl(data.properties);
             console.log(html);
             $('#school-info-wrapper').html(html);
         });
         klp.router.start();
-
     };
 
 })();
