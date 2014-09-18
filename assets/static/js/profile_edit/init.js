8017e4dc050cf0f58294135c72e6a6f75d00bd42
Sanjay B <b@pad.ma>
2014-09-18 18:24:43 +0530

    edit user profile basic details working

--- assets/static/js/profile_edit/init.js
+++ assets/static/js/profile_edit/init.js
@@ -1,8 +1,38 @@
 (function() {
     klp.init = function() {
-        var $profileXHR = klp.api.authDo("users/" + USER_ID);
+        var $profileXHR = klp.api.authDo("users/profile");
         $profileXHR.done(function(data) {
             console.log("data", data);
+            $('#firstName').val(data.first_name);
+            $('#lastName').val(data.last_name);
+            $('#email').val(data.email);
+            $('#mobileNo').val(data.mobile_no);
+
+            $('#userProfileForm').submit(function(e) {
+                if (e) {
+                    e.preventDefault();
+                }
+                var data = {
+                    'first_name': $('#firstName').val(),
+                    'last_name': $('#lastName').val(),
+                    'email': $('#email').val(),
+                    'mobile_no': $('#mobileNo').val()
+                };
+                //FIXME: do validations
+                var editXHR = klp.api.authDo("users/profile", data, "PATCH");
+                editXHR.done(function(response) {
+                    console.log("saved", response);
+                    klp.auth.loginUser(response);
+                });
+                editXHR.fail(function(err) {
+                    console.log("error saving", err);
+                });
+            });
+
+            $('#submitBtn').click(function(e) {
+                e.preventDefault();
+                $('#userProfileForm').submit();
+            });
         });
     };
 })();
