3665f676155e8add92dcde6254745a356b42731a
Bibhas <iambibhas@gmail.com>
2014-09-20 00:50:33 +0530

    added organization-user-adding-thingy to admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, User, UserOrganization
+
+class UserOrganizationInline(admin.TabularInline):
+    model = UserOrganization
+    extra = 1
+
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = (UserOrganizationInline,)
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(User)
