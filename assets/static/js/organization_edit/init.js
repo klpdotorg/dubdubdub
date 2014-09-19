1d9c283dcfd4cdc5ab6bbf44e30d72c27bd1f997
Sanjay B <b@pad.ma>
2014-09-19 13:50:51 +0530

    organization details edit

--- assets/static/js/organization_edit/init.js
+++ assets/static/js/organization_edit/init.js
@@ -1,8 +1,47 @@
 (function() {
     klp.init = function() {
-        var $orgXHR = klp.api.authDo("organizations/" + ORGANIZATION_ID);
+        var apiURL = "organizations/" + ORGANIZATION_ID;
+        var $orgXHR = klp.api.authDo(apiURL);
         $orgXHR.done(function(data) {
             console.log("data", data);
+            $('#orgName').val(data.name);
+            $('#orgURL').val(data.url);
+            $('#orgEmail').val(data.email);
+            $('#orgContactName').val(data.contact_name);
+            var formID = 'orgForm';
+            $('#' + formID).submit(function(e) {
+                if (e) {
+                    e.preventDefault();
+                }
+                klp.utils.clearValidationErrors(formID);
+                var fields = {
+                    'name': $('#orgName'),
+                    'url': $('#orgURL'),
+                    'email': $('#orgEmail'),
+                    'contact_name': $('#orgContactName')
+                };
+
+                if (klp.utils.validateRequired(formID)) {
+                    var data = klp.utils.getFormData(fields);
+                    var orgXHR = klp.api.authDo(apiURL, data, "PUT");
+                    orgXHR.done(function(response) {
+                        $('#displayOrgName').text(response.name);
+                        klp.utils.alertMessage("Changes saved", "success")
+                    });
+
+                    orgXHR.fail(function(err) {
+                        var errors = JSON.parse(err.responseText);
+                        klp.utils.invalidateErrors(fields, errors);
+                        //console.log("error saving", err);
+                    });
+                }
+
+            });
+
+            $('#submitBtn').click(function(e) {
+                e.preventDefault();
+                $('#' + formID).submit();
+            });
         });
     };
 })();
