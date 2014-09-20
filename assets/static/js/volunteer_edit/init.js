66d93e5e3581f5e28e0a2dc02dc0931a8ac2b16f
Sanjay B <b@pad.ma>
2014-09-20 17:04:19 +0530

    show activity users / change status

--- assets/static/js/volunteer_edit/init.js
+++ assets/static/js/volunteer_edit/init.js
@@ -2,10 +2,21 @@
     klp.init = function() {
         var $infoXHR = klp.api.authDo("volunteer_activities/" + VOLUNTEER_ACTIVITY_ID);
         var tplActivityDetails = swig.compile($('#tpl-activityDetails').html());
+        var tplVolunteerDetails = swig.compile($('#tpl-volunteerDetails').html());
         $infoXHR.done(function(data) {
             var detailsHTML = tplActivityDetails(data);
             $('#activityDetails').html(detailsHTML);
             console.log("data", data);
+
+            var userCount = data.users.length;
+
+            if (userCount > 0) {
+                _(data.users).each(function(user) {
+                    var userHTML = tplVolunteerDetails(user);
+                    $('#volunteerTable').append(userHTML);
+                });
+            }
+
             $('#activityType').val(data.type);
             $('#school').val(data.school);
             $('#organization').val(data.organization);
@@ -44,6 +55,30 @@
                     klp.utils.invalidateErrors(fields, errors);
                 });
 
+            });
+        });
+
+        $(document).on("click", ".js-volunteerVerify", function(e) {
+            e.preventDefault();
+            var $this = $(this);
+            var $parent = $this.parents('.js-volunteerTR');
+            var currentStatus = $this.attr("data-status");
+            var newStatus = currentStatus === "1" ? 0 : 1;
+            var userID = $parent.attr("data-id");
+            //console.log("user id", userID);
+            var url = "volunteer_activities/" + VOLUNTEER_ACTIVITY_ID + "/users/" + userID;
+            var data = {
+                'status': newStatus
+            };
+            var $statusXHR = klp.api.authDo(url, data, "PATCH");
+            $statusXHR.done(function(response) {
+                var html = tplVolunteerDetails(response);
+                $parent.replaceWith(html);
+                klp.utils.alertMessage("Saved change", "success");
+            });
+            $statusXHR.fail(function(err) {
+                klp.utils.alertMessage("An error occurred", "error");
+                //console.log("fail", err);
             });
         });
 
