43bd9c795405f00fd360e1c8644981703e2ba1f2
Bibhas <iambibhas@gmail.com>
2014-09-22 17:20:00 +0530

    returning questions based on school_type without questiongroup for now #156

---
+++ apps/stories/serializers.py
@@ -0,0 +1,25 @@
+from common.serializers import KLPSerializer, KLPSimpleGeoSerializer
+from rest_framework import serializers
+from schools.models import (School, Boundary, DiseInfo, ElectedrepMaster,
+    BoundaryType, Assembly, Parliament, Postal, PaisaData, MdmAgg)
+from .models import Question, Questiongroup, QuestionType, QuestiongroupQuestions
+
+
+class QuestionSerializer(KLPSerializer):
+    question_type = serializers.CharField(source='question_type.name')
+    options = serializers.SerializerMethodField('get_options')
+
+    class Meta:
+        model = Question
+        fields = ('question_type', 'text', 'qid', 'options')
+
+    def get_options(self, obj):
+        return obj.options.replace('{', '').replace('}', '').split(',') if obj.options else None
+
+
+class SchoolQuestionsSerializer(KLPSerializer):
+    questions = QuestionSerializer(many=True, source='get_questions')
+
+    class Meta:
+        model = School
+        fields = ('id', 'name', 'questions')
