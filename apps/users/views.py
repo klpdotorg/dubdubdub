57b7ad387d6ec0b7b1cc43da1363a9d3a2aea410
Bibhas <iambibhas@gmail.com>
2014-09-23 19:36:57 +0530

    added email verification when registering

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,33 @@
 from django.views.generic.detail import DetailView
+from django.http import Http404
+from common.views import StaticPageView
 from models import User, Organization, VolunteerActivity, VolunteerActivityType
 
 class ProfilePageView(DetailView):
     model = User
     template_name = 'profile.html'
+
+
+class EmailVerificationView(StaticPageView):
+    template_name = 'email_verified.html'
+
+    def get(self, request, **kwargs):
+        email_verification_code = self.request.GET.get('token', '')
+        email = self.request.GET.get('email', '')
+
+        users = User.objects.filter(
+            is_email_verified=False,
+            email=email,
+            email_verification_code=email_verification_code
+        )
+        if users.count() == 1:
+            user = users[0]
+            user.is_email_verified = True
+            user.save()
+        else:
+            raise Http404()
+
+        return super(EmailVerificationView, self).get(request, **kwargs)
 
 
 class OrganizationPageView(DetailView):
