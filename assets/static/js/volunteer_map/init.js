8e98fea3dd752c5bbe22ec5d8d791ea215e7cbe7
Sanjay B <b@pad.ma>
2014-09-23 19:30:19 +0530

    get silly date dropdown to work

--- assets/static/js/volunteer_map/init.js
+++ assets/static/js/volunteer_map/init.js
@@ -2,7 +2,44 @@
 
     klp.init = function() {
         klp.volunteer_map.init();
-
+        init_volunteer_date_filter();
     };
+
+    function init_volunteer_date_filter(){
+        console.log("initting date filter");
+        var $date_input = $("#vol-date-input");
+        var $calender_wrapper = $('.js-volunteer-cal');
+        var $datepicker_wrapper = $calender_wrapper.find("#vol-datepicker-wrapper");
+
+        $date_input.Zebra_DatePicker({
+            always_visible: $datepicker_wrapper,
+            format: 'd-m-Y',
+            first_day_of_week: 0,
+            show_clear_date: false,
+            show_select_today: false,
+            disabled_dates: ['1,3,4,5,12,13,14,22,24'],
+            onSelect: onDateSelect,
+            onClear: onDateClear
+        });
+     // var datepicker = $datepicker_input.data('Zebra_DatePicker');
+
+        function onDateSelect(date){
+            $date_input.val(date);
+            $calender_wrapper.removeClass("show");
+         // $btn_next_step_cal.show();
+        }
+
+        function onDateClear(){
+            $date_input.val("");
+            $btn_next_step_cal.hide();
+        }
+
+
+        $(document).on("click", ".js-volunter-date-input-wrapper", function(){
+            $(".js-volunteer-cal").toggleClass("show");
+        });
+
+    }
+
 
 })();
