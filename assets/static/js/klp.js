c3c14688e4a64e3126219592e2699c95aa495dba
Sanjay B <b@pad.ma>
2014-09-29 15:53:34 +0530

    remove inline js from base.html (move to js/klp.js)

--- assets/static/js/klp.js
+++ assets/static/js/klp.js
@@ -12,6 +12,49 @@
                 klp.openModal = null;
             }
         });
+
+        // Adds easing scrolling to # targets
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+                || location.hostname == this.hostname) {
+
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top-100
+                }, 300);
+                return false;
+              }
+            }
+        });
+
+        // Re-display top navigation if it gets hidden.
+        $(window).resize(function() {
+            if($(window).width() >=980){
+                $("#navigation").show();
+            } else {
+                $("#navigation").hide();
+            }
+        });
+
+        // Top navigation show dropdown on hover
+        $(".top-nav ul li" ).hover(
+          function() {
+            $( this ).find('ul').show();
+          }, function() {
+            $( this ).find('ul').hide();
+          }
+        );
+
+        $("#page_sticky_nav").stickOnScroll({
+            topOffset: 0,
+            setParentOnStick:   true,
+            setWidthOnStick:    true
+        });
+
+
+        
         klp.auth.init();
         klp.login_modal.init();
         if (klp.hasOwnProperty('init')) {
