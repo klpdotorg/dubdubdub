f955287e204e99290d0cdc23b94ec9f5c26269f5
Bibhas <iambibhas@gmail.com>
2014-11-25 20:15:41 +0530

    enabled searching stories in admin by school id

--- apps/stories/admin.py
+++ apps/stories/admin.py
@@ -20,10 +20,24 @@ class StoryAdmin(admin.ModelAdmin):
     list_display = ('email', 'entered_timestamp', 'school', 'is_verified',)
     list_editable = ('is_verified',)
     list_filter = ('is_verified',)
-    search_fields = ('school', 'school_id')
+    search_fields = ('school__name',)
     raw_id_fields = ('school',)
     ordering = ['-entered_timestamp']
     inlines = [AnswerInline, StoryImageInline]
+
+    def get_search_results(self, request, queryset, search_term):
+        """Searching school_id: By default django doesn't support integer
+        search using search_fields
+        """
+        queryset, use_distinct = super(StoryAdmin, self).get_search_results(
+            request, queryset, search_term)
+        try:
+            search_term_as_int = int(search_term)
+        except ValueError:
+            pass
+        else:
+            queryset |= self.model.objects.filter(school_id=search_term_as_int)
+        return queryset, use_distinct
 
 
 class StoryImageAdmin(admin.ModelAdmin):
