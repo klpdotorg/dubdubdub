d1e5b6d3fc526ccbb2d1906d8723e5e3b4eb4fd1
Haris Ibrahim K. V <blucalvin@gmail.com>
2014-11-28 18:58:25 +0530

    Implements management command to fetch today's IVRS results

---
+++ apps/stories/management/commands/fetchivrs.py
@@ -0,0 +1,23 @@
+import requests
+
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    args = "<dates month year>"
+    help = """Import data from IVRS
+
+    ./manage.py fetchivrs 12,13,14 12 2014"""
+
+    def handle(self, *args, **options):
+        if args:
+            pass
+        else:
+            today = timezone.now().date().strftime("%m/%d/%Y")
+            json = fetch_data(today)
+        
+    def fetch_data(date):
+        url = "http://89.145.83.72/akshara/json_feeds.php?fromdate=%s&enddate=%s" \
+              % (date, date)
+        response = requests.get(url)
+        return response.json()
