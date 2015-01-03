2505276674c19d119a952b8d32195e5314361ccb
Sanjay B <b@pad.ma>
2015-01-03 14:44:15 +0530

    add some more boilerplate js - function to make API call

--- assets/static/js/ivrs/init.js
+++ assets/static/js/ivrs/init.js
@@ -3,6 +3,19 @@
 
     klp.init = function() {
         console.log("initting app");
+        renderData();
     };
 
+    function renderData() {
+        //TODO: collect all form params
+        var params = {
+            'source': 'ivrs'
+        };
+        var url = "stories/meta";
+        var $xhr = klp.api.do(url, params);
+        $xhr.done(function(data) {
+            //TODO: Render data from back-end onto template
+            console.log(data);
+        });
+    }
 })();
