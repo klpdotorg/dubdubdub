dd6f933c3efb452d8f61c91ce4363ac93c245603
Sanjay B <b@pad.ma>
2015-01-09 16:11:30 +0530

    WIP: render data onto front-end

--- assets/static/js/ivrs/init.js
+++ assets/static/js/ivrs/init.js
@@ -3,19 +3,39 @@
 
     klp.init = function() {
         console.log("initting app");
-        renderData();
+        fetchData();
     };
 
-    function renderData() {
+    function fetchData() {
         //TODO: collect all form params
         var params = {
-            'source': 'ivrs'
+            'source': 'ivrs',
+            'school_type': 'Primary School'
         };
         var url = "stories/meta";
         var $xhr = klp.api.do(url, params);
         $xhr.done(function(data) {
             //TODO: Render data from back-end onto template
-            console.log(data);
+            renderData(data);
+
         });
+    }
+
+    function renderData(data) {
+        var questions = data.questions;
+        $('#num_response_this_week_n').text(data.total_responses);
+        for (var i=0; i<questions.length; i++) {
+            var question = questions[i];
+            renderQuestionChart(question, i);
+        }
+    }
+
+    function renderQuestionChart(questionData, number) {
+        var questionType = questionData.question_type;
+        var question = questionData.question;
+        var answers = questionData.answers;
+        var order = number + 1;
+        var $container = $('#question' + order);
+
     }
 })();
