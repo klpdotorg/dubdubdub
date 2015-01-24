45ba284ba0e928c0d0b3e7b67c5fb747d801a7cc
Karen Menezes <karen.menezes@gmail.com>
2015-01-24 16:02:29 +0530

    #396 code cleanups: removed show dropdown js

--- assets/static/js/donation_add_edit/init.js
+++ assets/static/js/donation_add_edit/init.js
@@ -3,23 +3,6 @@
         console.log("init called");
         //initialize form
         klp.donation_form.init();
-
-        //some random click handlers for the settings menu to the right of items
-        $(document).on("click", ".action-dropdown .dropdown-trigger", function(e){
-            if($(document).width() < 768){
-                return true;
-            }
-
-            e.preventDefault();
-
-            var $trigger = $(this).closest(".action-dropdown");
-
-            if(!$trigger.hasClass("show-drop")) {
-                $trigger.addClass("show-drop");
-            } else {
-                $trigger.removeClass("show-drop");
-            }
-        });
-    }
+    };
 
 })();
