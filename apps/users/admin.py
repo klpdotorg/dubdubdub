8f8e63283ebfd685de2ebcfc10acf85453f08910
Sanjay B <b@pad.ma>
2015-01-29 17:36:53 +0530

    make mark_published a method of UserAdmin and add some confirmation message

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -11,16 +11,23 @@ class OrganizationAdmin(admin.ModelAdmin):
     inlines = (UserOrganizationInline,)
 
 
-def mark_email_verified(modeladmin, request, queryset):
-    queryset.update(is_email_verified=True)
-
-mark_email_verified.short_description = "Mark selected users as verified"
 
 class UserAdmin(admin.ModelAdmin):
     search_fields = ('email', 'first_name', 'last_name')
     list_display = ('__unicode__', 'is_email_verified',)
-    actions = [mark_email_verified]
+    actions = ['mark_email_verified']
     #list_editable = ('is_email_verified',)
+    
+    def mark_email_verified(self, request, queryset):
+        rows_updated = queryset.update(is_email_verified=True)
+        if rows_updated == 1:
+            user_string = 'user'
+        else:
+            user_string = 'users'
+        message = "%s %s were marked as verified" % (rows_updated, user_string,)
+        self.message_user(request, message)
+
+    mark_email_verified.short_description = "Mark selected users as verified"
 
 
 
