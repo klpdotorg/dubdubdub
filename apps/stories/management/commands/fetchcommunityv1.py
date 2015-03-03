c45d2195ffc8d6cf004bc9400b04ae0e904ba62f
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-03-03 13:48:40 +0530

    Implements 'at_least_one_answer' restraint. Creates a Story only if at least one answer is present for the respective UserType in a given row

--- apps/stories/management/commands/fetchcommunityv1.py
+++ apps/stories/management/commands/fetchcommunityv1.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
             year = row[3]
             name = row[6]
             school_id = row[10]
-            acceptable_answers = ['0','1']
+            accepted_answers = ['0','1']
 
             # Skip first two rows
             if count == 0 or count == 1:
@@ -50,76 +50,103 @@ class Command(BaseCommand):
                 continue
                 
             # One for the TEACHERS
-            story, created = Story.objects.get_or_create(
-                school=school,
-                group=question_group,
-                date_of_visit=timezone.make_aware(
-                    date_of_visit, timezone.get_current_timezone()
-                ),
-                user_type=UserType.objects.get(name=UserType.TEACHERS)
-            )
-
             teachers_question_sequence = [1, 2, 3, 4, 5, 6, 7, 8]
             teachers_answer_columns = [13, 20, 27, 34, 42, 43, 44, 45]
-            for sequence_number, answer_column in zip(teachers_question_sequence, teachers_answer_columns):
+
+            for answer_column in teachers_answer_columns:
                 if row[answer_column] in accepted_answers:
-                    question = Question.objects.get(
-                        school_type=school.admin3.type,
-                        questiongroup__source=source,
-                        questiongroupquestions__sequence=sequence_number,
-                    )
-                    answer = Answer.objects.get_or_create(
-                        story=story,
-                        question=question,
-                        text=row[answer_column],
-                    )
+                    at_least_one_answer = True
+                    break
+            else:
+                at_least_one_answer = False
+
+            if at_least_one_answer:
+                story, created = Story.objects.get_or_create(
+                    school=school,
+                    group=question_group,
+                    date_of_visit=timezone.make_aware(
+                        date_of_visit, timezone.get_current_timezone()
+                    ),
+                    user_type=UserType.objects.get(name=UserType.TEACHERS)
+                )
+
+                for sequence_number, answer_column in zip(teachers_question_sequence, teachers_answer_columns):
+                    if row[answer_column] in accepted_answers:
+                        question = Question.objects.get(
+                            school_type=school.admin3.type,
+                            questiongroup__source=source,
+                            questiongroupquestions__sequence=sequence_number,
+                        )
+                        answer = Answer.objects.get_or_create(
+                            story=story,
+                            question=question,
+                            text=row[answer_column],
+                        )
                 
             # One for the PARENTS
-            story, created = Story.objects.get_or_create(
-                school=school,
-                group=question_group,
-                date_of_visit=timezone.make_aware(
-                    date_of_visit, timezone.get_current_timezone()
-                ),
-                user_type=UserType.objects.get(name=UserType.PARENTS)
-            )
-
             parents_question_sequence = [1, 2, 3, 4, 9, 10, 11, 12]
             parents_answer_columns = [14, 21, 28, 35, 48, 49, 50, 51]
-            for sequence_number, answer_column in zip(parents_question_sequence, parents_answer_columns):
+
+            for answer_column in parents_answer_columns:
                 if row[answer_column] in accepted_answers:
-                    question = Question.objects.get(
-                        school_type=school.admin3.type,
-                        questiongroup__source=source,
-                        questiongroupquestions__sequence=sequence_number,
-                    )
-                    answer = Answer.objects.get_or_create(
-                        story=story,
-                        question=question,
-                        text=row[answer_column],
-                    )
+                    at_least_one_answer = True
+                    break
+            else:
+                at_least_one_answer = False
+
+            if at_least_one_answer:
+                story, created = Story.objects.get_or_create(
+                    school=school,
+                    group=question_group,
+                    date_of_visit=timezone.make_aware(
+                        date_of_visit, timezone.get_current_timezone()
+                    ),
+                    user_type=UserType.objects.get(name=UserType.PARENTS)
+                )
+
+                for sequence_number, answer_column in zip(parents_question_sequence, parents_answer_columns):
+                    if row[answer_column] in accepted_answers:
+                        question = Question.objects.get(
+                            school_type=school.admin3.type,
+                            questiongroup__source=source,
+                            questiongroupquestions__sequence=sequence_number,
+                        )
+                        answer = Answer.objects.get_or_create(
+                            story=story,
+                            question=question,
+                            text=row[answer_column],
+                        )
 
             # And one for the VOLUNTEER who lives down the lane
-            story, created = Story.objects.get_or_create(
-                school=school,
-                group=question_group,
-                date_of_visit=timezone.make_aware(
-                    date_of_visit, timezone.get_current_timezone()
-                ),
-                user_type=UserType.objects.get(name=UserType.VOLUNTEER)
-            )
-
             communitys_question_sequence = [1, 2, 3, 4, 13, 14, 15, 16]
             communitys_answer_columns = [15, 22, 29, 36, 54, 55, 56, 57]
-            for sequence_number, answer_column in zip(communitys_question_sequence, communitys_answer_columns):
+
+            for answer_column in communitys_answer_columns:
                 if row[answer_column] in accepted_answers:
-                    question = Question.objects.get(
-                        school_type=school.admin3.type,
-                        questiongroup__source=source,
-                        questiongroupquestions__sequence=sequence_number,
-                    )
-                    answer = Answer.objects.get_or_create(
-                        story=story,
-                        question=question,
-                        text=row[answer_column],
-                    )
+                    at_least_one_answer = True
+                    break
+            else:
+                at_least_one_answer = False
+
+            if at_least_one_answer:
+                story, created = Story.objects.get_or_create(
+                    school=school,
+                    group=question_group,
+                    date_of_visit=timezone.make_aware(
+                        date_of_visit, timezone.get_current_timezone()
+                    ),
+                    user_type=UserType.objects.get(name=UserType.VOLUNTEER)
+                )
+
+                for sequence_number, answer_column in zip(communitys_question_sequence, communitys_answer_columns):
+                    if row[answer_column] in accepted_answers:
+                        question = Question.objects.get(
+                            school_type=school.admin3.type,
+                            questiongroup__source=source,
+                            questiongroupquestions__sequence=sequence_number,
+                        )
+                        answer = Answer.objects.get_or_create(
+                            story=story,
+                            question=question,
+                            text=row[answer_column],
+                        )
