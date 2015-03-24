4f4bd95e83e8d017304867f290b66d5bdb7e0f71
subhaklp <subha@klp.org.in>
2015-03-24 17:29:27 +0530

    Testing changes

--- runtests.sh
+++ runtests.sh
@@ -3,6 +3,9 @@
 # You will need to create a "test" db by cloning the main databasee (eg. createdb -T klpwww_ver4 test_klpwww_ver4 )
 #The name of the "test" DB should be entered in the dubdubdub/local_test_settings.py file. Please see those files for further instructions. 
 
+# Create the test database from the dev DB
+# Adding some test comments <DELETE THIS>
+#sudo -u postgres createdb -T klpwww_ver4 test_klpwww_ver4
 echo "Running schools related unit tests.."
 python manage.py test schools.tests --settings dubdubdub.test_settings
 echo "Done with schools unit tests ----------"
@@ -12,3 +15,4 @@ echo "Done with users unit tests............."
 echo "Running stories related unit tests"
 python manage.py test stories --settings dubdubdub.test_settings
 echo "Done with stories unit tests"
+
