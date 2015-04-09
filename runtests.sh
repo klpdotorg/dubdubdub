cc8d3d71ba824accf3e7c30d8c3ec0526d578867
subhaklp <subha@klp.org.in>
2015-04-09 09:12:23 +0530

    Creating and deleting test db as part of running tests

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,8 @@
 #This script runs all the existing unit tests for the users, schools and stories apps.
 # You will need to create a "test" db by cloning the main databasee (eg. createdb -T klpwww_ver4 test_klpwww_ver4 )
 #The name of the "test" DB should be entered in the dubdubdub/local_test_settings.py file. Please see those files for further instructions. 
-
+echo "Creating database"
+sudo -i -u postgres createdb -T klpwww_ver4 test_klpwww_ver4
 echo "Running schools related unit tests.."
 python manage.py test schools.tests --settings dubdubdub.test_settings
 echo "Done with schools unit tests ----------"
@@ -12,3 +13,5 @@ echo "Done with users unit tests............."
 echo "Running stories related unit tests"
 python manage.py test stories --settings dubdubdub.test_settings
 echo "Done with stories unit tests"
+echo "Deleting test database.."
+sudo -i -u postgres dropdb test_klpwww_ver4
