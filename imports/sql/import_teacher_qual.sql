32a52b07667180bfcb10ca396ff47d6c03e2d0ee
Bibhas <iambibhas@gmail.com>
2015-04-24 10:33:04 +0530

    fixed teacher_qual script

--- imports/sql/import_teacher_qual.sql
+++ imports/sql/import_teacher_qual.sql
@@ -6,9 +6,15 @@ BEGIN
     -- there is no primary key, so can't update anything
     FOR r IN
         SELECT ems_tb_teacher_qual.staff_id AS ems_id,
-               tb_teacher_qual.tid AS www_id
+               ems_tb_teacher_qual.qualification as ems_qualification,
+               tb_teacher_qual.tid AS www_id,
+               tb_teacher_qual.qualification as www_qualification
         FROM ems_tb_teacher_qual
-        LEFT JOIN tb_teacher_qual ON ems_tb_teacher_qual.staff_id=tb_teacher_qual.tid
+        LEFT JOIN tb_teacher_qual ON
+            (
+                ems_tb_teacher_qual.staff_id=tb_teacher_qual.tid AND
+                ems_tb_teacher_qual.qualification=tb_teacher_qual.qualification
+            )
         WHERE tb_teacher_qual.tid IS NULL
     LOOP
         RAISE NOTICE 'NEW TEACHER QUAL: %', r.ems_id;
@@ -16,7 +22,8 @@ BEGIN
             ems.staff_id,
             ems.qualification
         FROM ems_tb_teacher_qual as ems
-        WHERE ems.staff_id = r.ems_id;
+        WHERE ems.staff_id = r.ems_id AND
+            ems.qualification = r.ems_qualification;
     END LOOP;
     RETURN;
 END;
