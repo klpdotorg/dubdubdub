4fc26f72cc6e00f35a52614c9377e96cf42c1524
Bibhas <iambibhas@gmail.com>
2015-04-25 14:00:00 +0530

    added import commands

--- imports/importdatafromdb.sh
+++ imports/importdatafromdb.sh
@@ -20,4 +20,15 @@ done
 shift `expr $OPTIND - 1`
 
 sudo -u postgres psql -d $dbname -f imports/sql/import_boundary.sql
-python manage.py import_ems
+python manage.py import_ems # import schools and address
+sudo -u postgres psql -d $dbname -f imports/sql/import_child.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_class.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_student.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_student_class.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_teacher.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_teacher_class.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_teacher_qual.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_programme.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_assessment.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_question.sql
+sudo -u postgres psql -d $dbname -f imports/sql/import_student_eval.sql
