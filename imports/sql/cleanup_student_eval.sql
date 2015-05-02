9bfac23e22ab2f184601d5f48cfa47eb5a1b9c15
Bibhas <iambibhas@gmail.com>
2015-05-02 19:24:19 +0530

    modified sql for faster trimming

--- imports/sql/cleanup_student_eval.sql
+++ imports/sql/cleanup_student_eval.sql
@@ -1,7 +1,7 @@
 CREATE INDEX tb_student_eval_grade_idx ON tb_student_eval(grade);
 ANALYZE tb_student_eval;
 
-UPDATE tb_student_eval SET grade=TRIM(grade);
+UPDATE tb_student_eval SET grade=TRIM(both ' ' from grade) where grade LIKE '% ' OR grade LIKE ' %';
 
 UPDATE tb_student_eval SET grade='0' WHERE grade='000000' OR grade='000' OR grade='00' OR grade='.0' OR grade='0.';
 
