7d9c266940451cd72e42944ab56aa7584f839111
Shivangi <shivangidesai@Shivangis-MacBook-Air.local>
2015-05-15 15:08:16 +0530

    addition and correction of assessments data

---
+++ sql/correctionpid27.sql
@@ -0,0 +1,5 @@
+--pid 37 cohorts
+select fill_inst_assess_cohorts(121,'Anganwadi Class',125,ARRAY[125,148]);
+select fill_inst_assess_cohorts(121,'Anganwadi Class',148,ARRAY[125,148]);
+select fill_inst_assess_cohorts(121,'Anganwadi Class',149,ARRAY[126,149]);
+select fill_inst_assess_cohorts(121,'Anganwadi Class',126,ARRAY[126,149]);
