b6b40b9a57d3716aa1b4e774094fa1e7499cb484
Megha Vishwanath <megha@instascribe.com>
2015-05-26 22:14:47 +0530

    SYS form with stepy wizard

--- assets/static/js/sys/init.js
+++ assets/static/js/sys/init.js
@@ -9,7 +9,7 @@
         klp.router = new KLPRouter();
         klp.router.init();
         loadQuestions('schoolString',[]);
-
+        $('form').stepy();
     }
 
     
@@ -21,38 +21,56 @@
         $detailXHR.done(function(data) {*/
             
         //});
-        var questions = [
-                            {'question':'Was the school open?','id':1 },
-                            {'question':'Was the teacher present in each class?','id':2 },
-                            {'question':'Does the school have an all weather (pucca) building?','id':3 },
-                            {'question':'Are all the toilets in the school functional?','id':4 },
-                            {'question':'Does the school have a separate functional toilet for girls?','id':5 },
-                            {'question':'Does the school have drinking water?','id':6 },
-                            {'question':'Is a Mid Day Meal served in the school?','id':7 },
-                            {'question':'Is there a Boundary wall/ Fencing?','id':8 },
-                            {'question':'Is there a Play ground?','id':9 },
-                            {'question':'Is there Accessibility to students with disabilities?','id':10 },
-                            {'question':'Is there a Separate office for Headmaster?','id':11 },
-                            {'question':'Is there a Separate room as Kitchen / Store for Mid day meals?','id':12 },
-                            {'question':'Is there a Library?','id':13 },
-                            {'question':'Are there Play Materials or Sports Equipments?','id':14 },
-                            {'question':'Is there a Designated Librarian/Teacher?','id':15 },
-                            {'question':'Is there a Class-wise timetable for the Library?','id':16 },
-                            {'question':'Is there Teaching and Learning material?','id':17 },
-                            {'question':'Are there Sufficient number of class rooms?','id':18 },
-                            {'question':'Were at least 5% of the children enrolled present on the day you visited the school?','id':19 }
-                        ];
+        var questions = {
+            "academic":
+            [
+                {'question':'Was the school open?','id':1 },
+                {'question':'Was the teacher present in each class?','id':2 },
+                {'question':'Are there Sufficient number of class rooms?','id':17 },
+                {'question':'Were at least 50% of the children enrolled present on the day you visited the school?','id':19 },
+                {'question':'Is there a Separate office for Headmaster?','id':11 }
+            ],
+            "infra":
+            [
+                {'question':'Is there a Boundary wall/ Fencing?','id':8 },
+                {'question':'Is there Accessibility to students with disabilities?','id':10 },
+                {'question':'Is there a Play ground?','id':9 },
+                {'question':'Are there Play Materials or Sports Equipments?','id':14 }
+            ],   
+            "hygiene":
+            [            
+                {'question':'Are all the toilets in the school functional?','id':4 },
+                {'question':'Does the school have a separate functional toilet for girls?','id':5 },
+                {'question':'Does the school have drinking water?','id':6 },
+                {'question':'Is a Mid Day Meal served in the school?','id':7 },
+                {'question':'Is there a Separate room as Kitchen / Store for Mid day meals?','id':12 }
+            ],
+            "learning":
+            [
+                {'question':'Is there Teaching and Learning material?','id':16 },
+                {'question':'Is there a Library?','id':13 },
+                {'question':'Is there a Designated Librarian/Teacher?','id':15 }
+                
+            ]              
+        };
         var tplSysSchool = swig.compile($('#tpl-sysSchool').html());  
-        var html =  '<form action="/submitSys"><table class="table-base table-list-view table-base-sys">' +
-                    '<tbody><tr class="table-base-heading green-pista-bg">' +
-                    '<th class="white">All Observations below are mandatory</th>' +
+        var table_start =  '<table class="table-base table-list-view table-base-sys">' +
+                    '<tbody><tr class="table-base-heading">' +
+                    '<th class="grey-silver">Just a few questions more...</th>' +
                     '<th></th>' +
                     '<th></th>' +
                     '<th></th>' +
                     '</tr>';
-        html = html + tplSysSchool({'questions':questions});
-        html = html + '</tbody></table></form>';
-        $('#sysq_school').html(html);
+        var table_end = '</tbody></table>';
+
+        var html = table_start + tplSysSchool({'questions':questions["academic"]}) + table_end;      
+        $('#sysqset1_school').html(html);
+        html = table_start + tplSysSchool({'questions':questions["infra"]}) + table_end;      
+        $('#sysqset2_school').html(html);
+        html = table_start + tplSysSchool({'questions':questions["hygiene"]}) + table_end;      
+        $('#sysqset3_school').html(html);
+        html = table_start + tplSysSchool({'questions':questions["learning"]}) + table_end;      
+        $('#sysqset4_school').html(html);
     }
 
 })();
