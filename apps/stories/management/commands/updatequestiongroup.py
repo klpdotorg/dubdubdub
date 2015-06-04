b34f8e1c7648dd9129f83282f3529ffafa5bf1a9
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-06-04 10:47:05 +0530

    Adds docs for the random dates used

--- apps/stories/management/commands/updatequestiongroup.py
+++ apps/stories/management/commands/updatequestiongroup.py
@@ -21,11 +21,17 @@ class Command(BaseCommand):
                 group__source__name='web',
                 date_of_visit__year=2040
             )
+            # The one entry with date_of_visit as 2040 has the created_at
+            # time as 2010-09-10. So, we are using that.
             new_date = datetime.datetime.strptime('2010-09-10', '%Y-%m-%d')
             web_2040_story.date_of_visit = new_date
             web_2040_story.save()
         except:
             pass
+
+        # Certain date_of_visit values have the year as 1900. Since all community
+        # V2 data was collected after June 1st 2014, we are setting any earlier
+        # dates than 28th March 2014 to June 1st.
 
         earliest_allowed_date = datetime.datetime.strptime('2014-03-28', '%Y-%m-%d')
         v2_ancient_stories = Story.objects.filter(
