013f9ace3388ab9e19d041e5d4327cf22f999bea
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-03 22:49:44 +0530

    Implements the CheckSchool view

--- apps/ivrs/api_views.py
+++ apps/ivrs/api_views.py
@@ -1,10 +1,27 @@
+from rest_framework import status
 from rest_framework.response import Response
 
 from common.views import KLPAPIView, KLPDetailAPIView, KLPListAPIView
 
+from .models import State
 from schools.models import School, SchoolDetails
 
 
 class CheckSchool(KLPAPIView):
     def get(self, request):
-        return Response("Excellent", status=200, content_type='text/plain')
+        status_code = status.HTTP_200_OK
+
+        session_id = request.QUERY_PARAMS.get('CallSid', None)
+        if session_id:
+            state = State.objects.create(session_id=session_id)
+
+        school_id = request.QUERY_PARAMS.get('digits', None)
+        if school_id:
+            school_id = school_id.strip('"')
+            if School.objects.filter(id=school_id).exists():
+                state.school_id = school_id
+                state.save()
+            else:
+                status_code = status.HTTP_404_NOT_FOUND
+
+        return Response("", status=status_code)
