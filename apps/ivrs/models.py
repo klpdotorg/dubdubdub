b6e2131cadb6a3c0d6a17de4c4224c01332946fb
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-03 22:50:07 +0530

    Implements the State model and its migrations

--- apps/ivrs/models.py
+++ apps/ivrs/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class State(models.Model):
+    session_id = models.IntegerField(unique=True)
+    school_id = models.IntegerField(null=True, blank=True)
