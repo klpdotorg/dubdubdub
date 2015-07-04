c901ca857d863b9f5b29c368e4e760974f4334fa
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-04 23:48:59 +0530

    Adds the Verify URL

--- apps/ivrs/api_urls.py
+++ apps/ivrs/api_urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns(
     '',
     url(r'check-school/$', CheckSchool.as_view(), name='api_ivrs_check'),
     url(r'read-school/$', ReadSchool.as_view(), name='api_ivrs_read'),
+    url(r'verify/$', Verify.as_view(), name='api_ivrs_verify'),
 )
