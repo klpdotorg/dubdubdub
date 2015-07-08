ab496b5ca3f22e15802f3c72f7723ac3cc93423f
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-08 13:07:01 +0530

    Implements the get_question method

--- apps/ivrs/utils.py
+++ apps/ivrs/utils.py
@@ -1,5 +1,22 @@
+from .models import (
+    Question, Questiongroup
+)
+
 def get_options(question_number):
     if question_number == 2:
         return " Press 4 or 5 "
     else:
         return " Press 1 for Yes or 2 for No"
+
+def get_question(question_number):
+    question_group = Questiongroup.objects.get(
+        version=2,
+        source__name='ivrs'
+    )
+    question_number = question_number
+    question = Question.objects.get(
+        questiongroup=question_group
+        questiongroupquestions__sequence=question_number,
+    )
+
+    return question
