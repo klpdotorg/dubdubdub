c538dcbf98f9693f8efb28f02b1eb22c33fc86dd
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-08 12:30:40 +0530

    Adds the VerifyAnswer endpoint url

--- apps/ivrs/api_urls.py
+++ apps/ivrs/api_urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     url(r'check-school/$', CheckSchool.as_view(), name='api_ivrs_check'),
     url(r'read-school/$', ReadSchool.as_view(), name='api_ivrs_read'),
     url(r'verify/$', Verify.as_view(), name='api_ivrs_verify'),
+    url(r'verify-answer/$', VerifyAnswer.as_view(), name='api_ivrs_verify_answer'),
 )
