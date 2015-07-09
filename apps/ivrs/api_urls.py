f94976a8151994a92e07b2becb128b4cb85fd53b
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-09 10:58:22 +0530

    Adds the HangUp endpoint url

--- apps/ivrs/api_urls.py
+++ apps/ivrs/api_urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 
-from .api_views import CheckSchool, ReadSchool
+from .api_views import (
+    CheckSchool, ReadSchool, Verify, VerifyAnswer, HangUp
+)
 
 urlpatterns = patterns(
     '',
@@ -8,4 +10,5 @@ urlpatterns = patterns(
     url(r'read-school/$', ReadSchool.as_view(), name='api_ivrs_read'),
     url(r'verify/$', Verify.as_view(), name='api_ivrs_verify'),
     url(r'verify-answer/$', VerifyAnswer.as_view(), name='api_ivrs_verify_answer'),
+    url(r'hangup/$', HangUp.as_view(), name='api_ivrs_hangup'),
 )
