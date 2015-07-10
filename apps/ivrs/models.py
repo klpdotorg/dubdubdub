f32bfb879d4a3f10597b8d262da2654ba7bfd269
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-10 22:43:39 +0530

    Changes the session_id to a CharField

--- apps/ivrs/models.py
+++ apps/ivrs/models.py
@@ -5,7 +5,7 @@ from djorm_pgarray.fields import TextArrayField
 # Create your models here.
 
 class State(models.Model):
-    session_id = models.IntegerField(unique=True)
+    session_id = models.CharField(max_length=100,unique=True)
     school_id = models.IntegerField(null=True, blank=True)
     question_number = models.IntegerField(default=1)
     answers = TextArrayField(
