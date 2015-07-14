804349c30cd6f620a2f24f77eb12a30332591ec8
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-14 23:52:59 +0530

    Populates the script to save stories for each State in the db

--- apps/ivrs/management/commands/fetchgkaivrs.py
+++ apps/ivrs/management/commands/fetchgkaivrs.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand
 
+from .models import State
+from stories.models import Story, UserType, Questiongroup, Answer
+
 class Command(BaseCommand):
     args = ""
     help = """Analyzes the GKA IVRS states and saves stories.
@@ -8,4 +11,34 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
+        states = State.objects.all()
+        for state in states:
+            school = School.objects.get(id=state.school_id)
+            telephone = state.telephone
+            date = state.date_of_visit
+            akshara_staff = UserType.objects.get_or_create(
+                name=UserType.AKSHARA_STAFF
+            )[0]
+            question_group = Questiongroup.objects.get(
+                version=2,
+                source__name='ivrs'
+            )
+
+            story = Story.objects.create(
+                school=school,
+                group=question_group,
+                date_of_visit=date,
+                telephone=telephone,
+                user_type=akshara_staff
+            )
+
+            for (question_number, answer) in enumerate(state.answers):
+                if answer != 'NA':
+                    question = get_question(question_number+1)
+                    answer = Answer.objects.get_or_create(
+                        story=story,
+                        question=question,
+                        text=answer
+                    )
+
         return 1
