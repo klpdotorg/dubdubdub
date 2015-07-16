6d47cea0eac83c6626c5b327ca24fd8b45ebd5d9
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-16 17:52:22 +0530

    Removes the hangup endpoint

--- apps/ivrs/api_urls.py
+++ apps/ivrs/api_urls.py
@@ -13,5 +13,4 @@ urlpatterns = patterns(
     url(r'read-tlm/$', ReadTLM.as_view(), name='api_ivrs_read_tlm'),
     url(r'verify/$', Verify.as_view(), name='api_ivrs_verify'),
     url(r'verify-answer/$', VerifyAnswer.as_view(), name='api_ivrs_verify_answer'),
-    url(r'hangup/$', HangUp.as_view(), name='api_ivrs_hangup'),
 )
