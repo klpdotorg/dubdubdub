8b3dcee2d1bed69609d335edef91fd360a7032d4
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-07-22 07:36:16 +0530

    Retrieves unprocessed states

--- apps/ivrs/management/commands/fetchgkaivrs.py
+++ apps/ivrs/management/commands/fetchgkaivrs.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from django.core.management.base import BaseCommand
 
 from .models import State
@@ -11,34 +13,43 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        states = State.objects.all()
-        for state in states:
-            school = School.objects.get(id=state.school_id)
-            telephone = state.telephone
-            date = state.date_of_visit
-            akshara_staff = UserType.objects.get_or_create(
-                name=UserType.AKSHARA_STAFF
-            )[0]
-            question_group = Questiongroup.objects.get(
-                version=2,
-                source__name='ivrs'
-            )
-
-            story = Story.objects.create(
-                school=school,
-                group=question_group,
-                date_of_visit=date,
-                telephone=telephone,
-                user_type=akshara_staff
-            )
-
-            for (question_number, answer) in enumerate(state.answers):
-                if answer != 'NA':
-                    question = get_question(question_number+1)
-                    answer = Answer.objects.get_or_create(
-                        story=story,
-                        question=question,
-                        text=answer
+        fifteen_minutes = datetime.now() - timedelta(minutes=15)
+        states = State.objects.filter(
+            is_processed=False,
+            date_of_vist__lte=fifteen_minutes
+        )
+        if states:
+            for state in states:
+                if not sane_state(state):
+                    continue
+                else:
+                    school = School.objects.get(id=state.school_id)
+                    telephone = state.telephone
+                    date = state.date_of_visit
+                    akshara_staff = UserType.objects.get_or_create(
+                        name=UserType.AKSHARA_STAFF
+                    )[0]
+                    question_group = Questiongroup.objects.get(
+                        version=2,
+                        source__name='ivrs'
                     )
 
-        return 1
+                    story = Story.objects.create(
+                        school=school,
+                        group=question_group,
+                        date_of_visit=date,
+                        telephone=telephone,
+                        user_type=akshara_staff
+                    )
+
+                    for (question_number, answer) in enumerate(state.answers):
+                        if answer != 'NA':
+                            question = get_question(question_number+1)
+                            answer = Answer.objects.get_or_create(
+                                story=story,
+                                question=question,
+                                text=answer
+                            )
+
+def sane_state(state):
+    return 1
