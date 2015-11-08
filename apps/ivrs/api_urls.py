5a3fb3e4f06919f81ed3e8e44b83d5083784c60e
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-11-08 19:07:35 +0530

    Removes deprecated endpoints

--- apps/ivrs/api_urls.py
+++ apps/ivrs/api_urls.py
@@ -13,14 +13,6 @@ from .api_views import (
 
 urlpatterns = patterns(
     '',
-    url(r'verify/$',
-        Verify.as_view(),
-        name='api_ivrs_verify'
-    ),
-    url(r'read-tlm/$',
-        ReadTLM.as_view(),
-        name='api_ivrs_read_tlm'
-    ),
     url(r'check-school/$',
         CheckSchool.as_view(),
         name='api_ivrs_check'
@@ -28,10 +20,6 @@ urlpatterns = patterns(
     url(r'read-school/$',
         ReadSchool.as_view(),
         name='api_ivrs_read_school'
-    ),
-    url(r'read-chapter/$',
-        ReadChapter.as_view(),
-        name='api_ivrs_read_chapter'
     ),
     url(r'dynamic-response/$',
         DynamicResponse.as_view(),
