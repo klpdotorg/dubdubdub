a763222f9f60631235c1582a8c04eeee85276a62
Shivangi <shivangi@dev-klp-org-in.cloudmin.e2enetworks.net.in>
2015-12-12 20:57:09 +0530

    added the script

--- sql/run_markpercentile.sh
+++ sql/run_markpercentile.sh
@@ -1,3 +1,11 @@
+psql -U klp -d testdub -f deletequestions.sql
+echo "Finished delete questions"
+psql -U klp -d testdub -f assessment-aggregates.sql
+echo "Finished assessment-aggregates"
+psql -U klp -d testdub -f fillassessmentaggregates.sql
+echo "Finished fillassessmentaggregates"
+psql -U klp -d testdub -f fillboundaryassessments.sql
+echo "Finished fillboundaryassessments"
 psql -U klp -d testdub -f gradeassessment-aggregates.sql
 echo "Finished grade assessment aggregates"
 psql -U klp -d testdub -f fillgradeassessment-aggregates.sql
