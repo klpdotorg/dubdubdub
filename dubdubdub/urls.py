a270a94f981542763178d816bc3c3e359ffd2994
Bibhas <iambibhas@gmail.com>
2015-12-28 17:26:19 +0530

    district, mp and mla reports are available on adhoc dise report page

--- dubdubdub/urls.py
+++ dubdubdub/urls.py
@@ -61,12 +61,12 @@ urlpatterns = patterns(
     url(r'text/reports/$', RedirectView.as_view(url='/reports')),
 
     #temporary klp reports page to link to static pdfs
-    url(r'^klpreports/$', StaticPageView.as_view(
+    url(r'^reports/klp/$', StaticPageView.as_view(
         template_name='klpreports.html',
     ), name='klpreports'),
 
     #temporary dise reports page to link to static pdfs
-    url(r'^disereports/$', StaticPageView.as_view(
+    url(r'^reports/dise/$', StaticPageView.as_view(
         template_name='disereports.html',
     ), name='disereports'),
 
