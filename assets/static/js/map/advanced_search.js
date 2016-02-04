46297832828b07fb3abeb08c0c0888d772b504e3
Bibhas <iambibhas@gmail.com>
2016-02-04 20:08:26 +0530

    Advanced search form fixes

--- assets/static/js/map/advanced_search.js
+++ assets/static/js/map/advanced_search.js
@@ -2,17 +2,31 @@
 (function(){
     var t = klp.advanced_search = {};
 
-    var $_form_advanced_search;
+    var $_form_advanced_search,
+        $_input_partner;
 
     t.init = function(){
         $_form_advanced_search = $('#form_advanced_search');
         $_form_advanced_search.on('submit', search);
+
+        $_input_partner = $('input[name="partner"]:radio');
+        $_input_partner.on('change', fill_program);
     };
 
     var search = function(e) {
         e.preventDefault();
 
-        var data = $_form_advanced_search.serializeArray();
-        console.log(data);
+        var formdata = $_form_advanced_search.serializeArray();
+        var data = {};
+        $(formdata ).each(function(index, obj){
+            data[obj.name] = obj.value;
+        });
+        klp.router.setHash('advanced', data);
+    }
+
+    var fill_program = function(e) {
+        e.preventDefault();
+
+        console.log(e.target);
     }
 })();
