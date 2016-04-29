0baa0e7bef1db9a79c9d2bd2770050217790f06f
Megha Vishwanath <megha@klp.org.in>
2016-04-29 20:23:21 +0530

    New utils to parse parameters

---
+++ assets/static/js/reports/utils.js
@@ -0,0 +1,16 @@
+'use strict';
+(function() {
+  var t = klp.reportUtils = {};
+
+ t.getParameterByName  = function (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  
+})();
