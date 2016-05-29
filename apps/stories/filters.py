f57418d289f6669c8a4807c69734c6e2ddc1de99
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-05-29 17:58:45 +0530

    Adds the QuestionFilter

--- apps/stories/filters.py
+++ apps/stories/filters.py
@@ -1,3 +1,22 @@
 import django_filters
 
 from .models import Question
+
+class QuestionFilter(django_filters.FilterSet):
+    source = django_filters.MethodFilter(action='source_filter')
+    type = django_filters.MethodFilter(action='question_type_filter')
+
+    class Meta:
+        model = Question
+        fields = ['text', 'type', 'source']
+
+    def source_filter(self, queryset, value):
+        print value
+        return queryset.filter(
+            questiongroup__source__name=value
+        )
+
+    def question_type_filter(self, queryset, value):
+        return queryset.filter(
+            question_type__name=value
+        )
