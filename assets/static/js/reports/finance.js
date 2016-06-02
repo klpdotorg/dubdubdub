84fe382f3c11c9d3bdea02d190dbcde998aba4fb
Megha Vishwanath <megha@klp.org.in>
2016-06-02 08:03:30 +0530

    Changes to finance report

--- assets/static/js/reports/finance.js
+++ assets/static/js/reports/finance.js
@@ -30,34 +30,22 @@
         var $dataXHR = klp.api.do(detailURL, params);
         $datadetailXHR.done(function(data) {*/
             var grantdata = { 
+                "expected": {
+                    "grand_total": 5000000,
+                    "gender":{"per_girl":5, "total_girl":2000000, "per_boy":6, "total_boy": 3000000},
+                },
                 "received": {
-                    "sg_perc": 25,
-                    "sg_amt": 3500,
-                    "smg_perc": 65,
-                    "smg_amt": 5500,
-                    "tlm_perc": 10,
-                    "tlm_amt": 1000   
+                    "grand_total": 4500000, 
+                    "per_stu":6.5 
                 },
                 "expenditure": {
-                    "sg_perc": 35,
-                    "sg_amt": 3500,
-                    "smg_perc": 55,
-                    "smg_amt": 5500,
-                    "tlm_perc": 10,
-                    "tlm_amt": 1000 
+                    "grand_total": 4000000, 
+                    "per_stu":7.5  
                 }
             };
             renderGrants(grantdata);
             var allocdata = {
                 "sg":{
-                    "upper_primary" : { 
-                        "name":"Upper Primary",
-                        "school_count":200,
-                        "alloc_amt":"10,00,000",
-                        "alloc_perc":46.5,
-                        "per_school":"7,000",
-                        "per_student":"1.00"
-                    },
                     "lower_primary" : { 
                         "name":"Lower Primary",
                         "school_count":114,
@@ -65,24 +53,56 @@
                         "alloc_perc":53.5,
                         "per_school":"5,000",
                         "per_student":"0.75"
-                    }
-                },
-                "smg":{
-                   "more_than_3_cr" : { 
-                        "name":"With > 3 classrooms",
+                    },
+                    "upper_primary" : { 
+                        "name":"Upper Primary",
                         "school_count":200,
                         "alloc_amt":"10,00,000",
                         "alloc_perc":46.5,
                         "per_school":"7,000",
                         "per_student":"1.00"
-                    },
-                    "less_than_3_cr" :{ 
-                        "name":"With < 3 classrooms",
+                    }
+                },
+                "smg":{
+                    "upto_3_cr" :{ 
+                        "name":"With <= 3 classrooms",
                         "school_count":114,
                         "alloc_amt":"15,00,000",
                         "alloc_perc":53.5,
                         "per_school":"5,000",
                         "per_student":"0.75"
+                    },
+                   "4_cr" : { 
+                        "name":"With 4 classrooms",
+                        "school_count":200,
+                        "alloc_amt":"10,00,000",
+                        "alloc_perc":46.5,
+                        "per_school":"7,500",
+                        "per_student":"1.00"
+                    },
+                    "5_cr" : { 
+                        "name":"With 5 classrooms",
+                        "school_count":50,
+                        "alloc_amt":"2,50,000",
+                        "alloc_perc":15.5,
+                        "per_school":"10,000",
+                        "per_student":"1.00"
+                    }, //These below are in combination with category.
+                    "6_cr" : { 
+                        "name":"With 6-7 classrooms",
+                        "school_count":50,
+                        "alloc_amt":"2,50,000",
+                        "alloc_perc":15.5,
+                        "per_school":"15,000",
+                        "per_student":"1.00"
+                    },
+                    "8_cr" : { 
+                        "name":"With 8 classrooms",
+                        "school_count":20,
+                        "alloc_amt":"1,50,000",
+                        "alloc_perc":10.5,
+                        "per_school":"20,000",
+                        "per_student":"1.00"
                     }
                 },
                 "per":{
@@ -94,23 +114,16 @@
                     "per_teacher" : 500,
                 }
             }
-            renderAllocation(allocdata["sg"],'#sg-alloc');
-            renderAllocation(allocdata["smg"],"#smg-alloc");
-            renderPerChild(allocdata["per"],"#per-alloc");
+            renderAllocation(allocdata["sg"], "School Grant Allocation", '#sg-alloc');
+            renderAllocation(allocdata["smg"], "School Maintenance Allocation", "#smg-alloc");
             
             var comparisonJson = {
-                "year-wise": {
-                    "2013":{"year":"2013","per_student":4.5,"tlm":"5,00,000","sg_perc":45,"sg_amt":"20,00,000","smg_perc":35,"smg_amt":"30,00,000"},
-                    "2014":{"year":"2014","per_student":5,"tlm":"5,00,000","sg_perc":30,"sg_amt":"18,00,000","smg_perc":45,"smg_amt":"35,00,000"},
-                    "2015":{"year":"2015","per_student":5.5,"tlm":"5,00,000","sg_perc":50,"sg_amt":"23,00,000","smg_perc":55,"smg_amt":"40,00,000"}
-                }, /* maybe entrolment can be calculated with student and school count 
-                    Percentage could be percentage of schools in district */
                 "neighbours" : {
-                    "Bangalore Central":{"name":"Bangalore Central","per_student":4.5,"tlm":"5,00,000","sg_amt":"10,00,000","smg_amt":"32,00,000","total":"55,00,000","total_perc":45},
-                    "Bangalore North":{"name":"Bangalore North","per_student":5,"tlm":"4,00,000","sg_amt":"15,00,000","smg_amt":"34,00,000","total":"25,00,000","total_perc":55},
-                    "Bangalore South":{"name":"Bangalore South","per_student":4,"tlm":"3,00,000","sg_amt":"18,00,000","smg_amt":"35,00,000","total":"35,00,000","total_perc":65},
-                    "Bangalore Rural":{"name":"Bangalore Rural","per_student":3.5,"tlm":"5,00,000","sg_amt":"20,00,000","smg_amt":"36,00,000","total":"45,00,000","total_perc":35},
-                    "Chikkabalapur":{"name":"Chikkabalapur","per_student":4.6,"tlm":"4,00,000","sg_amt":"25,00,000","smg_amt":"37,00,000","total":"56,00,000","total_perc":25}
+                    "Bangalore Central":{"name":"Bangalore Central","expected":"25,00,000","received":"20,00,000","expenditure":"18,00,000","total_perc":45},
+                    "Bangalore North":{"name":"Bangalore North","expected":"25,00,000","received":"21,00,000","expenditure":"19,00,000","total_perc":55},
+                    "Bangalore South":{"name":"Bangalore South","expected":"25,00,000","received":"20,00,000","expenditure":"19,00,000","total_perc":65},
+                    "Bangalore Rural":{"name":"Bangalore Rural","expected":"25,00,000","received":"19,00,000","expenditure":"18,00,000","total_perc":35},
+                    "Chikkabalapur":{"name":"Chikkabalapur","expected":"25,00,000","received":"18,00,000","expenditure":"20,00,000","total_perc":25}
 
                 }
             }
@@ -138,62 +151,29 @@
 
     }
 
-function renderGrants(data){
-        drawStackedBar([ [data["expenditure"]["sg_perc"]],
-                         [data["expenditure"]["smg_perc"]],
-                         [data["expenditure"]["tlm_perc"]]
-                       ],"#chart-expenditure");
-        drawStackedBar([ [data["received"]["sg_perc"]],
-                         [data["received"]["smg_perc"]],
-                         [data["received"]["tlm_perc"]]
-                       ],"#chart-received");
+    function renderGrants(data){
+        var tpl = swig.compile($('#tpl-grantSummary').html());
+        var html = tpl({"data":data["expected"]});
+        $('#expected').html(html); 
+        
+        data["received"]["total_perc"] = Math.round(data.received.grand_total/data.expected.grand_total * 100);
+        data["received"]["perc_label"] = "expected";
+        html = tpl({"data":data["received"]});
+        $('#received').html(html); 
 
-        var tpl = swig.compile($('#tpl-grants').html());
-        var html = tpl({"grants":data["expenditure"]});
-        $('#dise-expenditure').html(html); 
-        html = tpl({"grants":data["received"]});
-        $('#dise-received').html(html);   
+        data["expenditure"]["total_perc"] = Math.round(data.expenditure.grand_total/data.received.grand_total * 100);
+        data["expenditure"]["perc_label"] = "received";
+        var html = tpl({"data":data["expenditure"]});
+        $('#expenditure').html(html);
+        
     }
 
-    function drawStackedBar(data, element_id) {
-        new Chartist.Bar(element_id, {
-                labels: [''],
-                series: data
-            }, {
-            stackBars: true,
-            horizontalBars: true,
-            axisX: {
-                showGrid: false
-            },
-            axisY: {
-                showGrid: false,
-                labelInterpolationFnc: function(value) {
-                return'';
-            }
-        }
-        }).on('draw', function(data) {
-            if(data.type === 'bar') {
-                data.element.attr({
-                    style: 'stroke-width: 20px'
-                });
-            }
-        });
-    }
-
-    function renderAllocation(data,element_id) {
+    function renderAllocation(data, heading, element_id) {
         var tplalloc = swig.compile($('#tpl-allocSummary').html());
-        var html = tplalloc({"data":data});
+        var html = tplalloc({"data":data,"heading":heading});
         $(element_id).html(html);     
     }
-    function renderPerChild(data,element_id) {
-        var tplalloc = swig.compile($('#tpl-perChild').html());
-        var html = tplalloc({"data":data});
-        $(element_id).html(html); 
-    }
     function renderComparison(data) {
-        var tplYearComparison = swig.compile($('#tpl-YearComparison').html()); 
-        var yrcompareHTML = tplYearComparison({"years":data["year-wise"]});
-        $('#comparison-year').html(yrcompareHTML);
         var tplComparison = swig.compile($('#tpl-neighComparison').html()); 
         var compareHTML = tplComparison({"neighbours":data["neighbours"]});
         $('#comparison-neighbour').html(compareHTML);
