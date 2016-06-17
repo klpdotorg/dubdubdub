46c889a21d15f7489057b1969ad95c77e3e2685d
Shivangi <shivangi@klp.org.in>
2016-06-17 14:21:09 +0530

    demographics dise report

--- assets/static/js/reports/demographics_dise.js
+++ assets/static/js/reports/demographics_dise.js
@@ -82,8 +82,16 @@
         .fail(function(err) {
             klp.utils.alertMessage("Sorry, could not fetch programmes data", "error");
         });
+        getNeighbourData(data, acadYear);
         
-        //getNeighbourData(data, acadYear)
+        var yearData = []; 
+        yearData[acadYear] = data["academic_year"];
+        var years = acadYear.split("-").map(Number);
+        yearData[(years[0]-1).toString()+"-"+(years[1]-1).toString()] = "20"+(years[0]-1).toString()+"-"+"20"+(years[1]-1).toString();
+        yearData[(years[0]-2).toString()+"-"+(years[1]-2).toString()] = "20"+(years[0]-2).toString()+"-"+"20"+(years[1]-2).toString();
+        var passYearData = {"name": data["boundary_info"]["name"], "type": data["boundary_info"]["type"]};
+        getMultipleData(yearData, passYearData, getLoopData, renderComparison,"acadYear");
+  
     }
 
 
@@ -147,6 +155,105 @@
         });
     }
 
+
+	function getNeighbourData(data, acadYear)
+    {
+        var type = data["boundary_info"]["type"];
+        if(type == "district")
+        {
+            klp.dise_api.getMultipleBoundaryData(null, null, type, acadYear).done(function(diseData) {
+                console.log('neighbours diseData', diseData);
+                renderNeighbours(diseData["results"]["features"]);
+            })
+            .fail(function(err) {
+                klp.utils.alertMessage("Sorry, could not fetch programmes data", "error");
+            });
+        }
+        else
+        {
+            klp.dise_api.queryBoundaryName(data["boundary_info"]["parent"]["name"], data["boundary_info"]["parent"]["type"],acadYear).done(function(diseData) {
+                var boundary = diseData[0].children[0];
+                klp.dise_api.getMultipleBoundaryData(boundary.id, boundary.type, type, acadYear).done(function(diseData) {
+                    console.log('neighbours diseData', diseData);
+                    renderNeighbours(diseData["results"]["features"]);
+                })
+                .fail(function(err) {
+                    klp.utils.alertMessage("Sorry, could not fetch programmes data", "error");
+                });
+            })
+            .fail(function(err) {
+                klp.utils.alertMessage("Sorry, could not fetch programmes data", "error");
+            });
+        }
+    }
+
+    function getMultipleData(inputData, passedData, getData, exit,iteratorName)
+    {
+        var numberOfIterations = Object.keys(inputData).length;
+        var outputData= {};
+        var index = 0,
+            done = false,
+            shouldExit = false;
+        var loop = {
+            next:function(){
+                if(done){
+                    if(shouldExit && exit){
+                        return exit(outputData); // Exit if we're done
+                    }
+                }
+                if(index <  numberOfIterations){
+                    index++; // Increment our index
+                    getData(loop); // Run our process, pass in the loop
+                }
+                else {
+                    done = true; // Make sure we say we're done
+                    if(exit)
+                        exit(outputData); // Call the callback on exit
+                }
+            },
+            iteration:function(){
+                passedData[iteratorName] = Object.keys(inputData)[index-1];
+                passedData["value"] = inputData[passedData[iteratorName]];
+                return passedData; // Return the loop number we're on
+            },
+            addData:function(diseData){
+                outputData[Object.keys(inputData)[index-1]] = diseData;
+            },
+            break:function(end){
+                done = true; // End the loop
+                shouldExit = end; // Passing end as true means we still call the exit callback
+            }
+        };
+        loop.next();
+        return loop;
+    }
+
+    function getLoopData(loop)
+    {
+        var data = loop.iteration();
+        klp.dise_api.queryBoundaryName(data["name"], data["type"], data["acadYear"]).done(function(diseNameData) {
+            if( diseNameData.length == 0)
+            {
+                loop.next();
+                return;
+            }
+            var boundary = diseNameData[0].children[0];
+            klp.dise_api.getBoundaryData(boundary.id, boundary.type, data["acadYear"]).done(function(diseData) {
+                console.log('diseData', diseData);
+                loop.addData(diseData);
+                loop.next();
+            })
+            .fail(function(err) {
+                klp.utils.alertMessage("Sorry, could not fetch programmes data", "for neighbour"+neighbour);
+            });
+        })
+        .fail(function(err) {
+            klp.utils.alertMessage("Sorry, could not fetch name data", "error");
+        });
+    }
+
+
+
     function renderCategories(categories) {
         var school_total = 0;
         for (var cat in categories) {
@@ -196,14 +303,68 @@
         
     }
 
-    function renderComparison(data) {
-        var tplYearComparison = swig.compile($('#tpl-YearComparison').html());
-        var yrcompareHTML = tplYearComparison({"years":data["year-wise"]});
-        $('#comparison-year').html(yrcompareHTML);
-        var tplComparison = swig.compile($('#tpl-neighComparison').html());
-        var compareHTML = tplComparison({"neighbours":data["neighbours"]});
+
+    function renderNeighbours(data){
+		var comparisonData = {};
+		var total_school_count = 0;
+		for( var itercount in data)
+		{
+			var iter = data[itercount];
+			comparisonData[iter["id"]] = {
+				"name": iter["id"],
+				"student_count": iter["properties"]["sum_girls"] + iter["properties"]["sum_boys"],
+				"school_perc": 0,
+                "teacher_count": iter["properties"]["sum_female_tch"] + iter["properties"]["sum_male_tch"],
+                "school_count": iter["properties"]["sum_schools"],
+			};
+			for(var cat in iter["properties"]["school_categories"]){
+				var type = iter["properties"]["school_categories"][cat];
+				if(type["id"] == 1)
+					comparisonData[iter["id"]]["enrol_lower"] = Math.round((type["sum_boys"] + type["sum_girls"])/comparisonData[iter["id"]]["school_count"]*100)/100;
+				else if(type["id"] == 2)
+					comparisonData[iter["id"]]["enrol_l_u"] = Math.round((type["sum_boys"] + type["sum_girls"])/comparisonData[iter["id"]]["school_count"]*100)/100;
+				else if(type["id"] == 4)
+					comparisonData[iter["id"]]["enrol_upper"] = Math.round((type["sum_boys"] + type["sum_girls"])/comparisonData[iter["id"]]["school_count"]*100)/100;
+			}
+			total_school_count += iter["properties"]["sum_schools"];
+			comparisonData[iter["id"]] ["ptr"] = Math.round(comparisonData[iter["id"]]["student_count"]/comparisonData[iter["id"]]["teacher_count"]*100)/100;
+		}
+		for(var neighbour in comparisonData)
+		{
+			comparisonData[neighbour]["school_perc"] = Math.round(comparisonData[neighbour]["school_count"]/total_school_count * 100)* 100/100;
+		}
+		console.log('comparisonData', comparisonData);
+		var tplComparison = swig.compile($('#tpl-neighComparison').html());
+        var compareHTML = tplComparison({"neighbours":comparisonData});
         $('#comparison-neighbour').html(compareHTML);
-    
+    }
+
+    function renderComparison(data) {
+		var comparisonData = {};
+		for( var itercount in data)
+		{
+			var iter = data[itercount];
+			comparisonData[itercount] = {
+				"year": "20"+itercount.toString().replace("-","-20"),
+				"student_count": iter["properties"]["sum_girls"] + iter["properties"]["sum_boys"],
+                "teacher_count": iter["properties"]["sum_female_tch"] + iter["properties"]["sum_male_tch"],
+                "school_count": iter["properties"]["sum_schools"],
+			};
+			for(var cat in iter["properties"]["school_categories"]){
+				var type = iter["properties"]["school_categories"][cat];
+				if(type["id"] == 1)
+					comparisonData[itercount]["enrol_lower"] = Math.round((type["sum_boys"] + type["sum_girls"])/comparisonData[itercount]["school_count"]*100)/100;
+				else if(type["id"] == 2)
+					comparisonData[itercount]["enrol_l_u"] = Math.round((type["sum_boys"] + type["sum_girls"])/comparisonData[itercount]["school_count"]*100)/100;
+				else if(type["id"] == 4)
+					comparisonData[itercount]["enrol_upper"] = Math.round((type["sum_boys"] + type["sum_girls"])/comparisonData[itercount]["school_count"]*100)/100;
+			}
+			comparisonData[itercount]["ptr"] = Math.round(comparisonData[itercount]["student_count"]/comparisonData[itercount]["teacher_count"]*100)/100;
+		}
+		
+        var tplYearComparison = swig.compile($('#tpl-YearComparison').html());
+        var yrcompareHTML = tplYearComparison({"years":comparisonData});
+        $('#comparison-year').html(yrcompareHTML);
     }
 
 })();
