ae2ee51c8070d8f5726fe39996230b540c345ad8
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-10-28 10:12:41 +0530

    Adds the 'invalid_data' field on the State model

--- apps/ivrs/models.py
+++ apps/ivrs/models.py
@@ -9,6 +9,7 @@ from stories.models import Questiongroup
 class State(models.Model):
     is_invalid = models.BooleanField(default=False)
     is_processed = models.BooleanField(default=False)
+    invalid_data = models.TextField(null=True, blank=True)
     school_id = models.IntegerField(null=True, blank=True)
     telephone = models.CharField(max_length=50, blank=True)
     date_of_visit = models.DateTimeField(default=timezone.now)
