21b39ad4a8c0190d5fbf42c521f1decee238f35f
Megha Vishwanath <megha@klp.org.in>
2016-11-29 19:19:31 +0530

    Changes to the import script

--- apps/schools/management/commands/import_meeting_pdfs.py
+++ apps/schools/management/commands/import_meeting_pdfs.py
@@ -3,6 +3,8 @@ from os.path import join, walk
 from optparse import make_option
 from schools.models import School, MeetingReport
 from django.core.files import File
+from datetime import datetime
+from common.utils import Date
 
 
 class Command(BaseCommand):
@@ -24,22 +26,29 @@ class Command(BaseCommand):
     def processDir(self, arg, dirname, fnames):
         for fname in fnames:
             #print fname
-            file_path = join(self.directory, dirname, fname)
+            #file_path = join(self.directory, dirname, fname)
+            file_path = join(dirname, fname)
             if file_path.endswith('.pdf'):
                 self.importPDF(file_path, fname)
 
 
     def importPDF(self, file_path, filename):
-        filename_split = filename.split("_")
-        school_id = filename_split[0]
-        language = filename_split[2].split(".")[0]
+        filepath_split = file_path.split('/')
+        print filepath_split
+        
+        filename_split = filename.rstrip('.pdf').split("_")
+        school_id = filename_split[1]
+        language = filepath_split[4] #6
         school = School.objects.get(pk=school_id)
         mr = MeetingReport()
         mr.school = school
         mr.language = language
-        fil = open(file_path)
-        django_file = File(fil)
-        mr.pdf.save(filename, django_file, save=True)
+        generated = Date()
+        mr.generated_at = generated.get_datetime(filepath_split[2]+'-'+filepath_split[3]+'-1')
+        #fil = open(file_path)
+        #django_file = File(fil)
+        #mr.pdf.save(file_path, django_file, save=True)
+        mr.pdf = file_path. lstrip('media/')
         mr.save()
 
 
