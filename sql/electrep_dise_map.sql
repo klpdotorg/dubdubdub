b8fbf660e344e051e74c5a24cdbb37d8b80cc0a2
Bibhas <iambibhas@gmail.com>
2016-12-21 02:46:34 +0530

    added parliament matching script

--- sql/electrep_dise_map.sql
+++ sql/electrep_dise_map.sql
@@ -36,13 +36,13 @@ dblink('host=localhost dbname=klpdise_olap user=klp'::text,
     'select assembly_name, slug from dise_1415_assembly_aggregations'::text)
     t(assembly_name text, slug text);
 
-select const_ward_type, const_ward_name, mvw_dise_assembly.assembly_name, mvw_dise_assembly.slug
-from tb_electedrep_master
-left outer join mvw_dise_assembly on (
-    jarowinkler(upper(const_ward_name), upper(mvw_dise_assembly.assembly_name)) > 0.92
-)
-where const_ward_type='MLA Constituency' and status='active' and dise_slug is null
-order by const_ward_name;
+-- select const_ward_type, const_ward_name, mvw_dise_assembly.assembly_name, mvw_dise_assembly.slug
+-- from tb_electedrep_master
+-- left outer join mvw_dise_assembly on (
+--     jarowinkler(upper(const_ward_name), upper(mvw_dise_assembly.assembly_name)) > 0.92
+-- )
+-- where const_ward_type='MLA Constituency' and status='active' and dise_slug is null
+-- order by const_ward_name;
 
 update tb_electedrep_master
 set
@@ -80,12 +80,31 @@ drop materialized view mvw_dise_assembly;
 ------------       PARLIAMENT       -----------------
 -----------------------------------------------------
 
+update tb_electedrep_master set dise_slug='dakshina-kannada' where status='active' and const_ward_type='MP Constituency' and const_ward_name='DAKSHINA';
+update tb_electedrep_master set dise_slug='udupi-chikmagalur' where status='active' and const_ward_type='MP Constituency' and const_ward_name='UDUPI';
+update tb_electedrep_master set dise_slug='uttara-kannada' where status='active' and const_ward_type='MP Constituency' and const_ward_name='UTTARA';
+
 create materialized view mvw_dise_parliament as
 select t.parliament_name, t.slug from
 dblink('host=localhost dbname=klpdise_olap user=klp'::text,
     'select parliament_name, slug from dise_1415_parliament_aggregations'::text)
     t(parliament_name text, slug text);
 
--- Do STUFF HERE!
+-- select const_ward_type, const_ward_name, mvw_dise_parliament.parliament_name, mvw_dise_parliament.slug
+-- from tb_electedrep_master
+-- left outer join mvw_dise_parliament on (
+--     jarowinkler(upper(const_ward_name), upper(mvw_dise_parliament.parliament_name)) > 0.95
+-- )
+-- where const_ward_type='MP Constituency' and status='active' and dise_slug is null
+-- order by const_ward_name;
+
+update tb_electedrep_master
+set
+    dise_slug=mvw_dise_parliament.slug
+from mvw_dise_parliament
+where jarowinkler(upper(const_ward_name), upper(mvw_dise_parliament.parliament_name)) > 0.95 and
+    const_ward_type='MP Constituency' and
+    status='active' and
+    dise_slug is null;
 
 drop materialized view mvw_dise_parliament;
