664bc823b7b41352420a3748acb3542a1ff5501d
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-02-28 08:42:42 +0530

    Comments out the IVRS call API urls

--- apps/ivrs/api_urls.py
+++ apps/ivrs/api_urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
 from .api_views import (
-    CheckSchool,
-    DynamicResponse,
-    ReadSchool,
+    # CheckSchool,
+    # DynamicResponse,
+    # ReadSchool,
     SMSView,
-    VerifyAnswer,
+    # VerifyAnswer,
 )
 
 urlpatterns = patterns(
@@ -14,21 +14,21 @@ urlpatterns = patterns(
         SMSView.as_view(),
         name='api_sms'
     ),
-    url(r'check-school/$',
-        CheckSchool.as_view(),
-        name='api_ivrs_check'
-    ),
-    url(r'read-school/$',
-        ReadSchool.as_view(),
-        name='api_ivrs_read_school'
-    ),
-    url(r'verify-answer/(?P<question_number>[-\w]+)/$',
-        VerifyAnswer.as_view(),
-        name='api_ivrs_verify_answer'
-    ),
-    # On hold
-    url(r'dynamic-response/$',
-        DynamicResponse.as_view(),
-        name='dynamic_response'
-    ),
+    # url(r'check-school/$',
+    #     CheckSchool.as_view(),
+    #     name='api_ivrs_check'
+    # ),
+    # url(r'read-school/$',
+    #     ReadSchool.as_view(),
+    #     name='api_ivrs_read_school'
+    # ),
+    # url(r'verify-answer/(?P<question_number>[-\w]+)/$',
+    #     VerifyAnswer.as_view(),
+    #     name='api_ivrs_verify_answer'
+    # ),
+    # # On hold
+    # url(r'dynamic-response/$',
+    #     DynamicResponse.as_view(),
+    #     name='dynamic_response'
+    # ),
 )
