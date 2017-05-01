17a58a9c28afcb93ebebc0e2e4fb64b565b9b606
Megha Vishwanath <megha@klp.org.in>
2017-05-01 22:58:15 +0530

    Added SMS and Assessments, GP contest remains

--- assets/static/js/gka/dashboard.js
+++ assets/static/js/gka/dashboard.js
@@ -65,11 +65,7 @@ var entityDetails = {};
         var queryParams = params.queryParams;
         //This is for the default URL localhost:8001/gka 
         //No Query Params
-        if(!window.location.hash)
-        {
-            loadData({});
-        }
-        else
+        if(window.location.hash)
         {
             //This is a reload of localhost:8001/gka 
             //No Query Params
@@ -91,28 +87,29 @@ var entityDetails = {};
     }
 
     function loadData(params) {
+        loadTopSummary(params);
         loadSmsData(params);
+        loadAssmtData(params);
+        
     }
 
     function loadSmsData(params) {
         var metaURL = "stories/meta/?source=sms";
-        startSummaryLoading();
         var $metaXHR = klp.api.do(metaURL, params);
+        startDetailLoading();
         $metaXHR.done(function(data) 
         {
-            stopSummaryLoading();
             renderSmsSummary(data);
         });
 
         //GETTING SMS DETAILS
         var detailURL = "stories/details/?source=sms";
         var $detailXHR = klp.api.do(detailURL, params);
-        startDetailLoading();
         $detailXHR.done(function(data) {
             stopDetailLoading();
             renderSMS(data);
         });
-        
+        renderSMSCharts(params);
     }
 
 
@@ -121,13 +118,16 @@ var entityDetails = {};
         var data = {
             "total_schools": 15000,
             "gka_schools":14854,
+            "children_impacted": 2000000,
             "tlm_kits":14854,
             "teachers_trained": 14000,
             "education_volunteers":200
         }
+        startSummaryLoading();
+        
         var tplTopSummary = swig.compile($('#tpl-topSummary').html());
-        var summaryData = data;
-        var topSummaryHTML = tplTopSummary(summaryData);
+        var topSummaryHTML = tplTopSummary({"data":data});
+        stopSummaryLoading(); 
         $('#topSummary').html(topSummaryHTML);
     }
 
@@ -138,6 +138,7 @@ var entityDetails = {};
         var smsSummaryHTML = tplSmsSummary(summaryData);
         $('#smsSummary').html(smsSummaryHTML);
     }
+
 
     function renderSMS(data) {
         var tplResponses = swig.compile($('#tpl-responseTable').html());
@@ -157,7 +158,218 @@ var entityDetails = {};
         });
         var questions = getQuestionsArray(questionObjects);
         var html = tplResponses({"questions":questions})
-        $('#smsquestions').html(html);
+        $('#smsQuestions').html(html);
+    }
+
+    function renderSMSCharts(params) {
+        var meta_values = [
+            {"meta":"CRP","value":130},
+            {"meta":"Field Staff","value": 100},
+            {"meta":"BEO","value":20},
+            {"meta":"Volunteer","value":35}
+        ];
+        var sms_sender = {
+            labels: ["CRP","Field Staff","BEO","Volunteer"],
+            series: [
+                { 
+                    className: 'ct-series-b',
+                    data: meta_values,
+                }
+            ],
+        }
+        renderBarChart('#smsSender', sms_sender);
+
+        var volume_values = [
+            {"meta":"Jun 2016","value":10},
+            {"meta":"Jul 2016","value":20},
+            {"meta":"Aug 2016","value":30},
+            {"meta":"Sep 2016","value":10},
+            {"meta":"Oct 2016","value":40},
+            {"meta":"Nov 2016","value":50},
+            {"meta":"Dec 2016","value":20},
+            {"meta":"Jan 2017","value":30},
+            {"meta":"Feb 2017","value":20},
+            {"meta":"Mar 2017","value":10}
+        ];
+
+        var sms_volume = {
+            labels: ["Jun 2016","Jul 2016","Aug 2016","Sep 2016","Oct 2016","Nov 2016","Dec 2016","Jan 2017","Feb 2017","Mar 2017"],
+            series: [
+                { 
+                    className: 'ct-series-b',
+                    data: volume_values,
+                }
+            ]
+        }
+        renderLineChart('#smsVolume', sms_volume);
+
+    }
+
+    function loadAssmtData(params) {
+        //var metaURL = "stories/meta/?source=sms";
+        //var $metaXHR = klp.api.do(metaURL, params);
+        // startDetailLoading();
+        // $metaXHR.done(function(data) 
+        // {
+        var data = {
+            "count": 2000,
+            "schools":148,
+            "schools_perc":10,
+            "children": 2000,
+            "children_perc":5,
+            "last_assmt":'30 Jan 2017 2:35:42 pm'
+        }
+     
+        renderAssmtSummary(data);
+
+        //});
+
+        renderAssmtCharts(params);
+    }
+
+    
+    function renderAssmtSummary(data) {
+        var tplAssmtSummary = swig.compile($('#tpl-assmtSummary').html());
+        var assmtSummaryHTML = tplAssmtSummary({'assmt':data});
+        $('#assmtSummary').html(assmtSummaryHTML);
+    }
+
+    function renderAssmtCharts(params) {
+        var meta_values = [
+            {"meta":"Addition","value":10},
+            {"meta":"Area of shape","value":20},
+            {"meta":"Carryover","value":50},
+            {"meta":"Decimals","value":10},
+            {"meta":"Division","value":40},
+            {"meta":"Division fact","value":60},
+            {"meta":"Double digit","value":50},
+            {"meta":"Fractions","value":90},
+            {"meta":"Place value","value":80},
+            {"meta":"Regrouping with money","value":70},
+            {"meta":"Subtraction","value":60},
+            {"meta":"Word problems","value":60}
+        ];
+        var competancies = {
+            labels: ["Addition","Area of shape","Carryover","Decimals","Division","Division fact","Double digit","Fractions","Place value","Regrouping with money","Subtraction","Word problems"],
+            series: [
+                { 
+                    //className: 'ct-series-b',
+                    data: meta_values,
+                    distributed_series:true
+                }
+            ],
+        }
+        renderBarChart('#assmtCompetancy', competancies);
+
+        var volume_values = [
+            {"meta":"Jun 2016","value":10},
+            {"meta":"Jul 2016","value":20},
+            {"meta":"Aug 2016","value":30},
+            {"meta":"Sep 2016","value":10},
+            {"meta":"Oct 2016","value":40},
+            {"meta":"Nov 2016","value":50},
+            {"meta":"Dec 2016","value":20},
+            {"meta":"Jan 2017","value":30},
+            {"meta":"Feb 2017","value":20},
+            {"meta":"Mar 2017","value":10}
+        ];
+
+        var assmt_volume = {
+            labels: ["Jun 2016","Jul 2016","Aug 2016","Sep 2016","Oct 2016","Nov 2016","Dec 2016","Jan 2017","Feb 2017","Mar 2017"],
+            series: [
+                { 
+                    className: 'ct-series-g',
+                    data: volume_values,
+                },
+                {
+                    className: 'ct-series-i',
+                    data: [60,60,60,60,60,60,60,60,60,60,60]  
+                }
+            ]
+        }
+        renderLineChart('#assmtVolume', assmt_volume);
+
+    }
+
+
+    function renderBarChart(elementId, data) {
+
+        var options = {
+            seriesBarDistance: 10,
+            axisX: {
+                showGrid: true,
+            },
+            axisY: {
+                showGrid: true,
+            },
+            plugins: [
+                Chartist.plugins.tooltip()
+            ]
+        };
+
+        var responsiveOptions = [
+            ['screen and (max-width: 640px)', {
+                seriesBarDistance: 5,
+                axisX: {
+                    labelInterpolationFnc: function (value) {
+                    return value;
+                }
+            }
+          }]
+        ];
+
+        var $chart_element = Chartist.Bar(elementId, data, options, responsiveOptions).on('draw', function(data) {
+            if (data.type === 'bar') {
+                data.element.attr({
+                    style: 'stroke-width: 15px;'
+                });
+            }
+            if (data.type === 'label' && data.axis === 'x') {
+                data.element.attr({
+                    width: 200
+                })
+            }
+        });
+    }
+
+    function renderLineChart(elementId, data) {
+
+        var options = {
+            seriesBarDistance: 10,
+            axisX: {
+                showGrid: true,
+            },
+            axisY: {
+                showGrid: true,
+            },
+            plugins: [
+                Chartist.plugins.tooltip()
+            ]
+        };
+
+        var responsiveOptions = [
+            ['screen and (max-width: 640px)', {
+                seriesBarDistance: 5,
+                axisX: {
+                    labelInterpolationFnc: function (value) {
+                    return value;
+                }
+            }
+          }]
+        ];
+
+        var $chart_element = Chartist.Line(elementId, data, options, responsiveOptions).on('draw', function(data) {
+            // if (data.type === 'bar') {
+            //     data.element.attr({
+            //         style: 'stroke-width: 15px;'
+            //     });
+            // }
+            if (data.type === 'label' && data.axis === 'x') {
+                data.element.attr({
+                    width: 200
+                })
+            }
+        });
     }
 
 
