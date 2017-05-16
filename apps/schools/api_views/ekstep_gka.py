573811e7a5486f6fcf1bc7a0ebb9ad7100aee149
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-05-16 22:26:55 +0530

    Implements the get_score function for the EkStepGKA class methods

--- apps/schools/api_views/ekstep_gka.py
+++ apps/schools/api_views/ekstep_gka.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
+
 from schools.models import AssessmentsV2
 
 # NOTE: For all the weird ['akshara.gka.'+str(number) for number in range(67, 88)]
-# bits, ask / refer the CSV sheets.
+# bits, ask / refer "the" CSV sheets.
 
 class EkStepGKA(object):
     def get_summary(self, assessments):
@@ -15,15 +17,42 @@ class EkStepGKA(object):
             'last_assmt':last_assessment_date,
         }
 
-    def get_addition_score(self, assessments):
-        question_ids = ['akshara.gka.'+str(number) for number in range(67, 88)]
-        assessments = assessments.filter(
-            question_id__in=question_ids)
-        # Calculate and return score.
+    def get_score(self, assessments, question_range):
+        question_ids = ['akshara.gka.'+str(number) for number in question_range]
+        assessments = assessments.filter(question_id__in=question_ids)
+
+        if set(question_range) == set(range(23, 44)):
+            correct_score = Decimal('2.0')
+        else:
+            correct_score = Decimal('1.0')
+
+        total_assessments = assessments.count()
+        total_correct_assessments = assessments.filter(
+            score=correct_score
+        ).count()
+
+        return {
+            'total':total_assessments,
+            'score':total_correct_assessments,
+        }
 
     def get_scores(self, assessments):
         scores = {}
-        scores['Addition'] = self.get_addition_score(assessments)
+        scores['Subtraction'] = self.get_score(assessments, range(1, 23))
+        scores['Division'] = self.get_score(assessments, range(23, 44))
+        scores['Double digit'] = self.get_score(assessments, range(44, 49))
+        scores['Place value'] = self.get_score(assessments, range(49, 54))
+        scores['Division fact'] = self.get_score(assessments, range(54, 59))
+        scores['Regrouping with money'] = self.get_score(assessments, range(59, 62))
+        scores['Carryover'] = self.get_score(assessments, range(62, 67))
+        scores['Addition'] = self.get_score(assessments, range(67, 88))
+        scores['Decimals'] = self.get_score(assessments, range(88, 89))
+        scores['Fractions'] = self.get_score(assessments, range(89, 90))
+        scores['Word problems'] = self.get_score(assessments, range(90, 91))
+        scores['Relationship between 3D shapes'] = self.get_score(assessments, range(91, 95))
+        scores['Area of shape'] = self.get_score(assessments, range(95, 99))
+
+        return scores
     
     def generate(self):
         response = {}
