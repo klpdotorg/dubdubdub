2dbfd74d654030dd8adb229f06fccc0623071213
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-07-12 06:08:58 +0530

    Merges the conflicting migrations

---
+++ apps/schools/migrations/0024_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('schools', '0023_auto_20170630_0642'),
+        ('schools', '0023_auto_20170619_1635'),
+    ]
+
+    operations = [
+    ]
