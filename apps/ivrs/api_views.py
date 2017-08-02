908ca65572cb0c865789f9f67a3cb02ab0092866
dataspect <mahajan.prajakta@gmail.com>
2017-08-02 09:40:52 +0530

    sms fixes for school_id and telephone

--- apps/ivrs/api_views.py
+++ apps/ivrs/api_views.py
@@ -39,7 +39,8 @@ class SMSView(KLPAPIView):
         parameters['raw_data'] = request.QUERY_PARAMS.get('Body', None)
         parameters['telephone'] = request.QUERY_PARAMS.get('From', None)
         parameters['session_id'] = request.QUERY_PARAMS.get('SmsSid', None)
-        parameters['telephone'] = parameters['telephone'][1:] # Strip 0
+        if parameters['telephone'] != None:
+                parameters['telephone'] = parameters['telephone'][1:] # Strip 0
 
         processed_data = process_data(parameters['raw_data'])
         incoming_number = IncomingNumber.objects.get(number=parameters['ivrs_type'])
@@ -49,7 +50,10 @@ class SMSView(KLPAPIView):
 
         is_invalid = True
 
-        if not is_user_registered(parameters['telephone']):
+	if parameters['telephone'] == None:
+            message = get_message(parameters, is_user_registered=False)
+
+        elif not is_user_registered(parameters['telephone']):
             message = get_message(parameters, is_user_registered=False)
 
         elif not is_data_valid(processed_data):
@@ -57,6 +61,9 @@ class SMSView(KLPAPIView):
 
         elif not is_logically_correct(processed_data):
             message = get_message(parameters, is_logically_correct=False)
+        
+        elif not school_id.isdigit():
+           message = get_message(parameters, is_school_id_valid=False)
 
         elif not is_school_exists(school_id):
             message = get_message(parameters, is_school_id_valid=False)
